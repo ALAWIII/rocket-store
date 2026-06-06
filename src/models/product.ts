b66eed2b6bd5cb88d7d0f2bfbc51975ea0b7618a
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-06 21:12:20 +0300

    feat: move description, info fields from Product to ProductVariant, as the Product must have at least one default Variant that holds the details information.

--- src/models/product.ts
+++ src/models/product.ts
@@ -14,14 +14,14 @@ class Product {
   name: string;
   brandId?: string;
   categoryId: string;
-  description: string;
-  info: Record<string, string>; // represented as key:value (JSONB) for which it can be substituted on the `details` html template.
   detailsHtml: string; // it must be much like an html page that is well designed to present the product details.
 }
 
 class ProductVariant {
   id: string;
   productId: string;
+  description: string;
+  info: Record<string, string>; // represented as key:value (JSONB) for which it can be substituted on the `details` html template.
   sku: string;
   price: number;
   quantity: number;
