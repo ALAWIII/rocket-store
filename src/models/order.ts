d901beab6222091689dc4f6e671db2c7ea7c63b0
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-07 15:33:42 +0300

    feat: add OrderStatus enum, add status field to Order.

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,15 +1,39 @@
 //----------------- after checkout ------------------
 // sku: Stock Keeping Unit. It’s an internal code used to identify and track a specific product variant in inventory, like a shirt in one size and color.
+
+import { ProductVariantId } from './product';
+enum OrderStatus {
+  Pending = 'pending',
+  Placed = 'placed',
+}
 class OrderItem {
-  variantId: string; // points to ProductVariant ID.
-  productName: string;
-  sku: string;
-  unitPrice: number;
-  quantity: number;
+  constructor(
+    private readonly id: string,
+    private readonly variantId: ProductVariantId,
+    private readonly productName: string,
+    private readonly sku: string,
+    private readonly unitPrice: number,
+    private readonly quantity: number,
+  ) {}
 }
 
-class Order {
-  id: string;
-  userId: string;
-  items: OrderItem[];
+export class Order {
+  private items: OrderItem[] = [];
+  private status: OrderStatus = OrderStatus.Pending;
+
+  constructor(
+    private readonly id: string,
+    private readonly userId: string,
+  ) {}
+
+  addItem(item: OrderItem) {
+    if (this.status !== OrderStatus.Pending)
+      throw new Error('Order already placed');
+    this.items.push(item);
+  }
+
+  placeOrder() {
+    if (this.items.length === 0) throw new Error('Order is empty');
+    this.status = OrderStatus.Placed;
+  }
 }
