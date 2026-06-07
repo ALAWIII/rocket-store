5b03752d38bc288d1d3e69270707d61bf04bb16d
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-07 14:43:33 +0300

    feat: add constructors and methods to cart,CartItem.
    CartItem class:
        - add increase, decrease methods for managing quantity.
        - add getters for ID's and quantity.
    
    Cart class: add addItem, removeItem, getItems methods.

--- src/models/cart.ts
+++ src/models/cart.ts
@@ -1,11 +1,61 @@
-//------------------ before checkout ---------------
+import { v7 } from 'uuid';
+import { ProductVariantId } from './product';
+
 class CartItem {
-  variantId: string;
-  quantity: number;
+  constructor(
+    private readonly id: string,
+    private readonly productVariantId: ProductVariantId,
+    private quantity: number,
+  ) {
+    if (quantity < 1) throw new Error('Quantity must be at least 1');
+  }
+
+  increase(quantity = 1) {
+    if (quantity < 1) throw new Error('Quantity must be positive');
+    this.quantity += quantity;
+  }
+
+  decrease(quantity = 1) {
+    if (quantity < 1) throw new Error('Quantity must be positive');
+    if (this.quantity - quantity < 1)
+      throw new Error('Quantity cannot be less than 1');
+    this.quantity -= quantity;
+  }
+
+  getProductVariantId() {
+    return this.productVariantId;
+  }
+
+  getQuantity() {
+    return this.quantity;
+  }
 }
 
 class Cart {
-  id: string;
-  userId: string;
-  items: CartItem[];
+  private items = new Map<ProductVariantId, CartItem>();
+
+  constructor(
+    private readonly id: string,
+    private readonly userId: string,
+  ) {}
+
+  addItem(productVariantId: ProductVariantId, quantity = 1) {
+    const existing = this.items.get(productVariantId);
+
+    if (existing) {
+      existing.increase(quantity);
+      return;
+    }
+
+    const item = new CartItem(v7(), productVariantId, quantity);
+    this.items.set(productVariantId, item);
+  }
+
+  removeItem(productVariantId: ProductVariantId) {
+    this.items.delete(productVariantId);
+  }
+
+  getItems() {
+    return [...this.items.values()];
+  }
 }
