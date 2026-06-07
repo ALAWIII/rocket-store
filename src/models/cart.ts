70c0b62699931b445c4509588b66b30764f628f5
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-07 15:06:32 +0300

    feat: add creatItem, getItem and update addItem implementation.

--- src/models/cart.ts
+++ src/models/cart.ts
@@ -10,44 +10,51 @@ class CartItem {
     if (quantity < 1) throw new Error('Quantity must be at least 1');
   }
 
-  increase(quantity = 1) {
+  increase(quantity = 1): void {
     if (quantity < 1) throw new Error('Quantity must be positive');
     this.quantity += quantity;
   }
 
-  decrease(quantity = 1) {
+  decrease(quantity = 1): void {
     if (quantity < 1) throw new Error('Quantity must be positive');
     if (this.quantity - quantity < 1)
       throw new Error('Quantity cannot be less than 1');
     this.quantity -= quantity;
   }
 
-  getProductVariantId() {
+  getProductVariantId(): string {
     return this.productVariantId;
   }
 
-  getQuantity() {
+  getQuantity(): number {
     return this.quantity;
   }
 }
 
-class Cart {
+export class Cart {
   private items = new Map<ProductVariantId, CartItem>();
 
   constructor(
     private readonly id: string,
     private readonly userId: string,
   ) {}
-
+  getItem(productVariantId: ProductVariantId): CartItem | undefined {
+    return this.items.get(productVariantId);
+  }
+  createItem(
+    productVariantId: ProductVariantId,
+    quantity: number = 1,
+  ): CartItem {
+    return new CartItem(v7(), productVariantId, quantity);
+  }
   addItem(productVariantId: ProductVariantId, quantity = 1) {
-    const existing = this.items.get(productVariantId);
+    const existing = this.getItem(productVariantId);
 
     if (existing) {
       existing.increase(quantity);
       return;
     }
-
-    const item = new CartItem(v7(), productVariantId, quantity);
+    const item = this.createItem(productVariantId);
     this.items.set(productVariantId, item);
   }
 
