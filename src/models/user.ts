d42d3f10063d3b5b77a37a87608e60542997977e
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-08 13:48:48 +0300

    feat: add constructor fields and methods.

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,21 +5,48 @@ enum RoleType {
   Custom = 'custom',
 }
 class Role {
-  id: string;
-  name: string; // unique
-  roleType: RoleType;
-  permissions: Set<Permission>;
+  permissions = new Set<Permission>();
+  constructor(
+    private readonly id: string,
+    private name: string, // unique
+    private roleType: RoleType,
+  ) {}
+  addPermission(permission: Permission) {
+    this.permissions.add(permission);
+  }
+  removePermission(permission: Permission) {
+    this.permissions.delete(permission);
+  }
+  setName(name: string) {
+    this.name = name;
+  }
 }
 
 class User {
-  id: string;
-  email: string;
-  passwordHash: string;
-  firstName: string;
-  lastName: string;
-  phone?: string;
-  roleId: string;
-  lastLoginAt?: Date;
-  createdAt: Date;
-  updatedAt: Date;
+  constructor(
+    private readonly id: string,
+    private email: string,
+    private passwordHash: string,
+    private firstName: string,
+    private lastName: string,
+    private roleId: string,
+    private readonly createdAt: Date = new Date(),
+    private updatedAt: Date,
+    private phone?: string,
+  ) {}
+  setEmail(email: string) {
+    this.email = email;
+  }
+  setPassword(password: string) {
+    this.passwordHash = password;
+  }
+  setPhone(phone: string) {
+    this.phone = phone;
+  }
+  setUpdatedAt(d: Date) {
+    this.updatedAt = d;
+  }
+  setRoleId(r: string) {
+    this.roleId = r;
+  }
 }
