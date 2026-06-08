730c41442b89cf8ccb48edf986ecc7990416b6a2
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-08 21:51:32 +0300

    feat: massive category changes and updates.
    - replace CategoryData with CreateCategoryProps and RestoreCategoryData types.
    - add static validateName method which throws error if the name doesnt match business rules.
    - update getters naming.
    - add lastUpdated,createdAt,updateDate getters and setters.
    - add toJson serializations.
    - add create,restore methods for constructing new Categories and restoring existing once.

--- src/products/domain/category.ts
+++ src/products/domain/category.ts
@@ -1,39 +1,108 @@
 export type CategoryId = string;
+type CreateCategoryProps = {
+  id: CategoryId;
+  name: string;
+  parentCategoryId?: CategoryId;
+};
+
+type RestoreCategoryData = {
+  id: CategoryId;
+  name: string;
+  parentCategoryId?: CategoryId;
+  createdAt: Date;
+  updatedAt: Date;
+};
 class Category {
-  private parentCategoryId?: CategoryId;
+  private constructor(
+    private data: {
+      id: CategoryId;
+      name: string;
+      parentCategoryId?: CategoryId;
+    },
+    readonly _createdAt: Date,
+    private updatedAt: Date,
+  ) {}
 
-  constructor(
-    private readonly id: CategoryId,
-    private name: string,
-  ) {
-    if (!name.trim()) throw new Error('Category name is required');
+  static create(data: CreateCategoryProps): Category {
+    const now = new Date();
+
+    return new Category(
+      {
+        id: data.id,
+        name: Category.validateName(data.name),
+        parentCategoryId: data.parentCategoryId,
+      },
+      now,
+      now,
+    );
   }
 
-  getId(): CategoryId {
-    return this.id;
+  static restore(data: RestoreCategoryData): Category {
+    return new Category(
+      {
+        id: data.id,
+        name: Category.validateName(data.name),
+        parentCategoryId: data.parentCategoryId,
+      },
+      data.createdAt,
+      data.updatedAt,
+    );
+  }
+  private static validateName(name: string): string {
+    const normalized = name.trim();
+    if (normalized.length < 2) {
+      throw new Error('Category name must be at least 2 characters.');
+    }
+    if (normalized.length > 20) {
+      throw new Error('Category name must not exceed 20 characters.');
+    }
+    return normalized;
+  }
+  get id(): CategoryId {
+    return this.data.id;
   }
 
-  getName(): string {
-    return this.name;
+  get name(): string {
+    return this.data.name;
   }
 
-  getParentCategoryId(): CategoryId | undefined {
-    return this.parentCategoryId;
+  get parentCategoryId(): CategoryId | undefined {
+    return this.data.parentCategoryId;
   }
 
   rename(name: string): void {
-    if (!name.trim()) throw new Error('Category name is required');
-    this.name = name;
+    this.data.name = Category.validateName(name);
+    this.updateDate();
   }
 
   assignParent(parentCategoryId: CategoryId): void {
     if (parentCategoryId === this.id) {
       throw new Error('Category cannot be parent of itself');
     }
-    this.parentCategoryId = parentCategoryId;
+    this.data.parentCategoryId = parentCategoryId;
+    this.updateDate();
   }
 
   removeParent(): void {
-    this.parentCategoryId = undefined;
+    this.data.parentCategoryId = undefined;
+    this.updateDate();
+  }
+  private updateDate() {
+    this.updatedAt = new Date();
+  }
+  get createdAt(): Date {
+    return new Date(this._createdAt);
+  }
+
+  get lastUpdated(): Date {
+    return new Date(this.updatedAt);
+  }
+
+  toJson() {
+    return {
+      ...this.data,
+      createdAt: this.createdAt,
+      lastUpdated: this.lastUpdated,
+    };
   }
 }
