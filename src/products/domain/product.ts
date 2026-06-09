348864aebb167a71f7f0ab3badd11f363d4fe43a
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-09 14:27:01 +0300

    feat: add some fields validators for products.
    - add ProductDetails,ProductName class validator.
    - update getters of Product class.

--- src/products/domain/product.ts
+++ src/products/domain/product.ts
@@ -12,22 +12,45 @@ type ProductVariantData = {
   description?: string;
 };
 type ProductData = {
-  id: ProductId;
-  name: string;
+  id: string;
+  name: ProductName;
+  detailsPage?: ProductDetails;
   categoryId: CategoryId;
-  detailsPage: string;
   brandId?: BrandId;
 };
-
+export class ProductName {
+  private constructor(private _name: string) {}
+  static create(name: string): ProductName {
+    const normalized = name.trim();
+    if (normalized.length < 2 || normalized.length > 100)
+      throw new Error(
+        'Invalid product name length must be between 2 and 100 characters.',
+      );
+    return new ProductName(normalized);
+  }
+  get name(): string {
+    return this._name;
+  }
+}
+export class ProductDetails {
+  private constructor(private _page: string) {}
+  static create(page: string): ProductDetails {
+    const normalized = page.trim();
+    if (normalized.length < 2)
+      throw new Error(
+        'Invalid product name length must be between 2 and 100 characters.',
+      );
+    return new ProductDetails(normalized);
+  }
+  get page(): string {
+    return this._page;
+  }
+}
 export class Product {
   private variants = new ProductVaraintList();
-  constructor(private productData: ProductData) {
-    if (!productData.name.trim()) throw new Error('Product name is required');
-    if (!productData.detailsPage.trim())
-      throw new Error('Details page is required');
-  }
+  constructor(private productData: ProductData) {}
 
-  getId(): ProductId {
+  get id(): ProductId {
     return this.productData.id;
   }
   createVariant(pvd: ProductVariantData) {
@@ -41,24 +64,23 @@ export class Product {
   removeVariant(id: ProductVariantId) {
     this.variants.remove(id);
   }
-  getName(): string {
-    return this.productData.name;
+  get name(): string {
+    return this.productData.name.name;
   }
 
-  getCategoryId(): CategoryId {
+  get categoryId(): CategoryId {
     return this.productData.categoryId;
   }
 
-  getBrandId(): BrandId | undefined {
+  get brandId(): BrandId | undefined {
     return this.productData.brandId;
   }
 
-  getDetailsPage(): string {
-    return this.productData.detailsPage;
+  get detailsPage(): string | undefined {
+    return this.productData.detailsPage?.page;
   }
 
-  rename(name: string): void {
-    if (!name.trim()) throw new Error('Product name is required');
+  rename(name: ProductName): void {
     this.productData.name = name;
   }
 
@@ -74,8 +96,7 @@ export class Product {
     this.productData.categoryId = categoryId;
   }
 
-  changeDetailsPage(detailsPage: string): void {
-    if (!detailsPage.trim()) throw new Error('Details page is required');
+  changeDetailsPage(detailsPage: ProductDetails): void {
     this.productData.detailsPage = detailsPage;
   }
 }
