e3af0833c30e93ab2cfaf38f4ba9bc8c36fb9bb7
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-12 21:04:25 +0300

    feat: update Cart models.
    - add CartItemProps and add userId field to it.
    - add set/get quantity for CartItem model.
    - remove increase,decreas and old setters,getters from CartItem.
    - remove createItem method and id field from Cart.
    - update addItem body logic.
    - add toJson for Cart and CartItem models.

--- src/carts/domain/cart.ts
+++ src/carts/domain/cart.ts
@@ -1,68 +1,52 @@
-import { ProductVariantId } from 'src/shared/domain/ids';
-import { v7 } from 'uuid';
+import { ProductVariantId, UserId } from 'src/shared/domain/ids';
 
-class CartItem {
-  constructor(
-    private readonly id: string,
-    private readonly productVariantId: ProductVariantId,
-    private quantity: number,
-  ) {
+type CartItemProps = {
+  readonly id: string;
+  readonly productVariantId: ProductVariantId;
+  userId: UserId;
+  quantity: number;
+};
+export class CartItem {
+  constructor(private props: CartItemProps) {
+    if (props.quantity < 1) throw new Error('Quantity must be at least 1');
+  }
+
+  get productVariantId(): string {
+    return this.props.productVariantId;
+  }
+
+  get quantity(): number {
+    return this.props.quantity;
+  }
+  set quantity(quantity: number) {
     if (quantity < 1) throw new Error('Quantity must be at least 1');
+    this.props.quantity = quantity;
   }
-
-  increase(quantity = 1): void {
-    if (quantity < 1) throw new Error('Quantity must be positive');
-    this.quantity += quantity;
-  }
-
-  decrease(quantity = 1): void {
-    if (quantity < 1) throw new Error('Quantity must be positive');
-    if (this.quantity - quantity < 1)
-      throw new Error('Quantity cannot be less than 1');
-    this.quantity -= quantity;
-  }
-
-  getProductVariantId(): string {
-    return this.productVariantId;
-  }
-
-  getQuantity(): number {
-    return this.quantity;
+  toJSON(): CartItemProps {
+    return { ...this.props };
   }
 }
 
 export class Cart {
-  private items = new Map<ProductVariantId, CartItem>();
+  constructor(private items: CartItem[] = []) {}
 
-  constructor(
-    private readonly id: string,
-    private readonly userId: string,
-  ) {}
-  getItem(productVariantId: ProductVariantId): CartItem | undefined {
-    return this.items.get(productVariantId);
-  }
-  createItem(
-    productVariantId: ProductVariantId,
-    quantity: number = 1,
-  ): CartItem {
-    return new CartItem(v7(), productVariantId, quantity);
-  }
-  addItem(productVariantId: ProductVariantId, quantity = 1) {
-    const existing = this.getItem(productVariantId);
+  addItem(itemProps: CartItemProps) {
+    const existing = this.getItem(itemProps.productVariantId);
 
     if (existing) {
-      existing.increase(quantity);
+      existing.quantity = itemProps.quantity;
       return;
     }
-    const item = this.createItem(productVariantId);
-    this.items.set(productVariantId, item);
+    const item = new CartItem(itemProps);
+    this.items.push(item);
+  }
+  getItem(productVariantId: ProductVariantId): CartItem | undefined {
+    return this.items.find(
+      (citem) => citem.productVariantId == productVariantId,
+    );
   }
 
-  removeItem(productVariantId: ProductVariantId) {
-    this.items.delete(productVariantId);
-  }
-
-  getItems() {
-    return [...this.items.values()];
+  toJSON(): CartItemProps[] {
+    return [...this.items].map((citem) => citem.toJSON());
   }
 }
