7147732e0c2e29efcf40f81282bbce43c3c94e63
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-13 14:16:30 +0300

    feat: update Order,OrderItem implementations.
    - remove sku field from OrderItemProps.
    - add OrderItemProps, OrderProps types.
    - add restore,create static methods for Order.
    - update placeOrder, addItem implementation for Order.
    - add total method for OrderItem, Order.
    - add toJSON method for OrderItem.
    - add 'paid' option for OrderStatus.

--- src/orders/domain/order.ts
+++ src/orders/domain/order.ts
@@ -2,39 +2,63 @@
 // sku: Stock Keeping Unit. It’s an internal code used to identify and track a specific product variant in inventory, like a shirt in one size and color.
 
 import { ProductVariantId } from 'src/shared/domain/ids';
+import { v7 } from 'uuid';
 
 enum OrderStatus {
-  Pending = 'pending',
-  Placed = 'placed',
+  Pending = 'pending', // created, not yet confirmed
+  Placed = 'placed', // confirmed / payment initiated
+  Paid = 'paid', // payment succeeded
 }
-class OrderItem {
-  constructor(
-    private readonly id: string,
-    private readonly variantId: ProductVariantId,
-    private readonly productName: string,
-    private readonly sku: string,
-    private readonly unitPrice: number,
-    private readonly quantity: number,
-  ) {}
-}
+type OrderItemProps = {
+  readonly id: string;
+  readonly variantId: ProductVariantId;
+  readonly productTitle: string;
+  readonly unitPrice: number;
+  readonly quantity: number;
+};
 
+type OrderProps = {
+  readonly id: string;
+  readonly userId: string;
+  status: OrderStatus;
+  items: OrderItem[];
+};
 export class Order {
-  private items: OrderItem[] = [];
-  private status: OrderStatus = OrderStatus.Pending;
-
-  constructor(
-    private readonly id: string,
-    private readonly userId: string,
-  ) {}
-
-  addItem(item: OrderItem) {
-    if (this.status !== OrderStatus.Pending)
-      throw new Error('Order already placed');
-    this.items.push(item);
+  private constructor(private props: OrderProps) {}
+  static create(userId: string): Order {
+    return new Order({
+      id: v7(),
+      userId: userId,
+      status: OrderStatus.Pending,
+      items: [],
+    });
+  }
+  static restore(props: OrderProps): Order {
+    return new Order(props);
+  }
+  addItem(itemProps: OrderItemProps) {
+    this.props.items.push(new OrderItem(itemProps));
   }
 
   placeOrder() {
-    if (this.items.length === 0) throw new Error('Order is empty');
-    this.status = OrderStatus.Placed;
+    if (this.props.items.length === 0) throw new Error('Order is empty');
+    this.props.status = OrderStatus.Placed;
+  }
+  get total(): number {
+    let t = 0;
+    for (const citem of this.props.items) {
+      t += citem.total;
+    }
+    return t;
+  }
+}
+
+class OrderItem {
+  constructor(private props: OrderItemProps) {}
+  get total(): number {
+    return this.props.quantity * this.props.unitPrice;
+  }
+  toJSON(): OrderItemProps {
+    return { ...this.props };
   }
 }
