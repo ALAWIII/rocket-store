94f5049986b87c0d744295cd058f9a2a5bb99d9b
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-16 17:56:30 +0300

    feat: implement Payment logic and methods.
    - add CreatePaymentProps.
    - add create, restore, markAsPaid, markAsFailed, cancel, touch methods.

--- src/payments/domain/payment.ts
+++ src/payments/domain/payment.ts
@@ -17,7 +17,12 @@ const PaymentStatus = {
   CANCELLED: 'CANCELLED',
 } as const;
 type PaymentStatus = (typeof PaymentStatus)[keyof typeof PaymentStatus];
-
+type CreatePaymentProps = {
+  orderId: OrderId;
+  amountInMinorUnit: number;
+  currency: string;
+  method: PaymentMethod;
+};
 type PaymentProps = {
   id: PaymentId;
   orderId: OrderId;
@@ -29,6 +34,48 @@ type PaymentProps = {
   createdAt: Date;
   updatedAt: Date;
 };
+
+export class Payment {
+  private constructor(private props: PaymentProps) {}
+  static create(data: CreatePaymentProps): Payment {
+    const now = new Date();
+    return new Payment({
+      id: PaymentId.create(),
+      ...data,
+      createdAt: now,
+      updatedAt: now,
+      status: PaymentStatus.PENDING,
+    });
+  }
+  static restore(data: PaymentProps): Payment {
+    return new Payment(data);
+  }
+  markAsPaid(at: Date = new Date()) {
+    if (this.props.status === PaymentStatus.PAID) return;
+    this.props.status = PaymentStatus.PAID;
+    this.props.paidAt = at;
+    this.touch();
+  }
+
+  markAsFailed() {
+    if (this.props.status === PaymentStatus.PAID) {
+      throw new Error('Paid payment cannot be marked failed');
+    }
+    this.props.status = PaymentStatus.FAILED;
+    this.touch();
+  }
+
+  cancel() {
+    if (this.props.status === PaymentStatus.PAID) {
+      throw new Error('Paid payment cannot be cancelled');
+    }
+    this.props.status = PaymentStatus.CANCELLED;
+    this.touch();
+  }
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+}
 
 type PaymentTransactionProps = {
   id: PaymentTransactionId;
@@ -56,6 +103,5 @@ type PaymentProviderProps = {
   createdAt: Date;
   updatedAt: Date;
 };
-class Payment {}
 class PaymentTransaction {}
 class PaymentProvider {}
