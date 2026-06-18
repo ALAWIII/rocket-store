c7d717b15c8e336cc06f0af49ce351a71ee1ff06
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-18 16:07:18 +0300

    feat: add PromotionTargetType.

--- src/promotions/domain/promotion-target.ts
+++ src/promotions/domain/promotion-target.ts
@@ -1,7 +1,11 @@
-const PromotionTargetType = {
+import { ValueOf } from 'src/shared/types/value-of';
+
+export const PromotionTargetType = {
   PRODUCT: 'PRODUCT',
   CATEGORY: 'CATEGORY',
   BRAND: 'BRAND',
   ORDER: 'ORDER',
   SHIPPING: 'SHIPPING',
 } as const;
+
+export type PromotionTargetType = ValueOf<typeof PromotionTargetType>;
