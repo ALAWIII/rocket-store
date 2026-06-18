47811c5a1c93dd1f5a0bbd2e11088346d23717b6
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-18 19:26:34 +0300

    feat: add and implement all needed rules.

--- src/promotions/domain/promotion-rule-type.ts
+++ src/promotions/domain/promotion-rule-type.ts
@@ -1,29 +1,119 @@
-export const PROMOTION_RULE_TYPE = {
-  MIN_ORDER_AMOUNT: 'MIN_ORDER_AMOUNT',
-  MIN_QUANTITY: 'MIN_QUANTITY',
-  PRODUCT_IN_CART: 'PRODUCT_IN_CART',
-  CATEGORY_IN_CART: 'CATEGORY_IN_CART',
-  BRAND_IN_CART: 'BRAND_IN_CART',
-  FIRST_ORDER: 'FIRST_ORDER',
-  CUSTOMER_SEGMENT: 'CUSTOMER_SEGMENT',
-  SHIPPING_METHOD: 'SHIPPING_METHOD',
-  PAYMENT_METHOD: 'PAYMENT_METHOD',
-} as const;
-
 export abstract class PromotionRuleType<T> {
   abstract readonly kind: string;
+
   constructor(protected readonly props: T) {}
+
   toJSON() {
-    return { kind: this.kind, ruleData: { ...this.props } };
+    return {
+      kind: this.kind,
+      ruleData: { ...this.props },
+    };
   }
 }
-type MinOrderAmountProps = {
+
+export type MinOrderAmountProps = {
   amount: number;
   currency: string;
 };
+
 export class MinOrderAmount extends PromotionRuleType<MinOrderAmountProps> {
-  readonly kind: string = 'MIN_ORDER_AMOUNT';
+  readonly kind = 'MIN_ORDER_AMOUNT';
+
   constructor(props: MinOrderAmountProps) {
+    super(props);
+  }
+}
+
+export type MinQuantityProps = {
+  quantity: number;
+};
+
+export class MinQuantity extends PromotionRuleType<MinQuantityProps> {
+  readonly kind = 'MIN_QUANTITY';
+
+  constructor(props: MinQuantityProps) {
+    super(props);
+  }
+}
+
+export type ProductInCartProps = {
+  productIds: string[];
+};
+
+export class ProductInCart extends PromotionRuleType<ProductInCartProps> {
+  readonly kind = 'PRODUCT_IN_CART';
+
+  constructor(props: ProductInCartProps) {
+    super(props);
+  }
+}
+
+export type CategoryInCartProps = {
+  categoryIds: string[];
+};
+
+export class CategoryInCart extends PromotionRuleType<CategoryInCartProps> {
+  readonly kind = 'CATEGORY_IN_CART';
+
+  constructor(props: CategoryInCartProps) {
+    super(props);
+  }
+}
+
+export type BrandInCartProps = {
+  brandIds: string[];
+};
+
+export class BrandInCart extends PromotionRuleType<BrandInCartProps> {
+  readonly kind = 'BRAND_IN_CART';
+
+  constructor(props: BrandInCartProps) {
+    super(props);
+  }
+}
+
+export type FirstOrderProps = Record<string, never>;
+
+export class FirstOrder extends PromotionRuleType<FirstOrderProps> {
+  readonly kind = 'FIRST_ORDER';
+
+  constructor(props: FirstOrderProps = {}) {
+    super(props);
+  }
+}
+
+export type CustomerSegmentProps = {
+  segmentIds: string[];
+};
+
+export class CustomerSegment extends PromotionRuleType<CustomerSegmentProps> {
+  readonly kind = 'CUSTOMER_SEGMENT';
+
+  constructor(props: CustomerSegmentProps) {
+    super(props);
+  }
+}
+
+export type ShippingMethodProps = {
+  shippingMethodIds: string[];
+};
+
+export class ShippingMethod extends PromotionRuleType<ShippingMethodProps> {
+  readonly kind = 'SHIPPING_METHOD';
+
+  constructor(props: ShippingMethodProps) {
+    super(props);
+  }
+}
+
+export type PaymentMethodProps = {
+  paymentMethodIds: string[];
+};
+
+export class PaymentMethod extends PromotionRuleType<PaymentMethodProps> {
+  readonly kind = 'PAYMENT_METHOD';
+
+  constructor(props: PaymentMethodProps) {
     super(props);
   }
 }
