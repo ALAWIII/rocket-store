5761318d304a848a7f86e3f9a1aa4d48ae5417f2
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-21 14:11:52 +0300

    feat: configure and add database module import to app module.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'allawiii',
+      password: '0788',
+      database: 'store',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+})
 export class DatabaseModule {}
