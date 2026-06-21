9d6d6194001964108a5702f4aa22b3b97f18bd97
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-21 20:41:22 +0300

    feat: add OrderAddress database entity.

--- src/modules/users/infrastructure/entities/address.entity.ts
+++ src/modules/users/infrastructure/entities/address.entity.ts
@@ -6,9 +6,12 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
+  Unique,
   UpdateDateColumn,
 } from 'typeorm';
 import { UserEntity } from './user.entity';
+import { AddressType } from '../../domain/address';
+import { OrderEntity } from 'src/modules/orders/infrastructure/entities/order.entity';
 
 @Entity('addresses')
 export class Address {
@@ -54,4 +57,44 @@ export class Address {
   @ManyToOne(() => UserEntity)
   @JoinColumn({ name: 'user_id' })
   user!: UserEntity;
+}
+
+@Entity('order_addresses')
+@Unique(['orderId', 'addressType'])
+class OrderAddress {
+  @UuidV7PrimaryColumn()
+  id!: string;
+  @Column({ type: 'varchar', length: 20, enum: AddressType })
+  addressType!: AddressType;
+  @Column('varchar', { length: 50 })
+  fullName!: string;
+
+  @Column('varchar', { length: 15 })
+  phone!: string;
+
+  @Column('varchar', { length: 30 })
+  country!: string;
+
+  @Column('varchar', { length: 30 })
+  city!: string;
+
+  @Column('varchar', { length: 30 })
+  state!: string;
+
+  @Column('varchar', { length: 40 })
+  postalCode!: string;
+
+  @Column('varchar', { length: 50 })
+  addressLine1!: string;
+
+  @Column('varchar', { length: 50, nullable: true })
+  addressLine2?: string;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+  @Column({ type: 'uuid', name: 'order_id' })
+  orderId!: string;
+  @ManyToOne(() => OrderEntity, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'order_id' })
+  order!: OrderEntity;
 }
