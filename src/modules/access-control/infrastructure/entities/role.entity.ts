590d610e3112f9af8f1d09abb3b51aad357f28e1
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-21 18:02:01 +0300

    feat: update primary key decorator.

--- src/modules/access-control/infrastructure/entities/role.entity.ts
+++ src/modules/access-control/infrastructure/entities/role.entity.ts
@@ -1,8 +1,9 @@
-import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
+import { Entity, Column } from 'typeorm';
 
 @Entity('roles')
 export class RoleEntity {
-  @PrimaryColumn('uuid', { default: () => 'uuidv7' })
+  @UuidV7PrimaryColumn()
   id!: string;
   @Column({ type: 'varchar', length: 20, unique: true })
   name!: string;
