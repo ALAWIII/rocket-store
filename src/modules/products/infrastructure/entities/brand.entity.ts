fce8c51bfe3427f95f68b54fbff286322a0250b0
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-22 17:39:08 +0300

    feat: add brand database entity model.

---
+++ src/modules/products/infrastructure/entities/brand.entity.ts
@@ -0,0 +1,10 @@
+import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
+import { Column, Entity } from 'typeorm';
+
+@Entity('brands')
+export class BrandEntity {
+  @UuidV7PrimaryColumn()
+  id!: string;
+  @Column('varchar', { length: 30, unique: true })
+  name!: string;
+}
