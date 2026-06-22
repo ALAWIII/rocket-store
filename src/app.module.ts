649dd5ee1786a5ae979d3ad557064ad486fc15ab
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-22 17:44:17 +0300

    feat: add audit log module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { AuditLogModule } from './modules/audit-log/audit-log.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuditLogModule],
 })
 export class AppModule {}
