828c3a323c71eb08062705364a8362f4a3ec878f
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-22 17:36:50 +0300

    feat: add product database entity.

---
+++ src/modules/products/infrastructure/entities/product.entity.ts
@@ -0,0 +1,17 @@
+import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
+import { Column, Entity, ForeignKey } from 'typeorm';
+import { BrandEntity } from './brand.entity';
+
+@Entity('products')
+export class ProductEntity {
+  @UuidV7PrimaryColumn()
+  id!: string;
+  @Column('varchar', { length: 100 })
+  title!: string;
+  @Column('varchar', { length: 500 })
+  description!: string;
+
+  @Column({ nullable: true })
+  @ForeignKey(() => BrandEntity, 'id')
+  brandId?: string | null;
+}
