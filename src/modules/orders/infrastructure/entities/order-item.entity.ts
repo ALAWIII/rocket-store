6bb099a04be6848c81daa2b36dc8ed6f6a9faa8f
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-22 14:26:55 +0300

    feat: add OrderItem database entity.

---
+++ src/modules/orders/infrastructure/entities/order-item.entity.ts
@@ -0,0 +1,29 @@
+import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
+import { Column, Entity, ForeignKey, JoinColumn, ManyToOne } from 'typeorm';
+import { OrderEntity } from './order.entity';
+import { ProductVariantEntity } from 'src/modules/products/infrastructure/entities/product-variant.entity';
+
+@Entity('order_items')
+export class OrderItemEntity {
+  @UuidV7PrimaryColumn()
+  id!: string;
+  @Column('varchar', { length: 100 })
+  productTitle!: string;
+  @Column('integer', { default: 0 })
+  unitPrice!: number;
+  @Column('integer', { default: 0 })
+  quantity!: number;
+  @Column('integer', { default: 0 })
+  subtotal!: number;
+  @Column({ type: 'uuid', name: 'order_id' })
+  @ForeignKey(() => OrderEntity, (o) => o.id, {
+    onDelete: 'CASCADE',
+  })
+  orderId!: string;
+
+  @Column({ type: 'uuid', name: 'product_variant_id' })
+  @ForeignKey(() => ProductVariantEntity, (p) => p.id, {
+    onDelete: 'RESTRICT',
+  })
+  productVariantId!: string;
+}
