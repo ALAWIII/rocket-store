f62f400ddb8d434a5065ed396fce588648dc5877
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-23 14:16:00 +0300

    feat: add page_templates database entity.

---
+++ src/modules/products/infrastructure/entities/page-template.entity.ts
@@ -0,0 +1,25 @@
+import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
+import { Column, CreateDateColumn, Entity, ForeignKey } from 'typeorm';
+import { CategoryEntity } from './category.entity';
+import { UserEntity } from 'src/modules/users/infrastructure/entities/user.entity';
+
+@Entity('page_templates')
+export class PageTemplateEntity {
+  @UuidV7PrimaryColumn()
+  id!: string;
+  @Column('varchar', { length: 500 })
+  name!: string;
+  @Column('jsonb')
+  editorState!: Record<string, unknown>;
+  @Column('text')
+  renderedHtml!: string;
+
+  @Column('uuid', { nullable: true })
+  @ForeignKey(() => CategoryEntity, (c) => c.id, { onDelete: 'SET NULL' })
+  categoryId?: string | null;
+  @Column('uuid')
+  @ForeignKey(() => UserEntity, (u) => u.id)
+  createdBy!: string;
+  @CreateDateColumn()
+  createdAt!: Date;
+}
