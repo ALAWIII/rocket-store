8ab849a67a8dabebc03d77c13558e682807d0a3f
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-23 15:41:34 +0300

    feat: add Cart database entity.

---
+++ src/modules/carts/infrastructure/entities/cart.entity.ts
@@ -0,0 +1,14 @@
+import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
+import { UserEntity } from 'src/modules/users/infrastructure/entities/user.entity';
+import { Column, CreateDateColumn, Entity, ForeignKey } from 'typeorm';
+
+@Entity('carts')
+export class Cart {
+  @UuidV7PrimaryColumn()
+  id!: string;
+  @Column('uuid')
+  @ForeignKey(() => UserEntity, (u) => u.id)
+  userId!: string;
+  @CreateDateColumn()
+  createdAt!: Date;
+}
