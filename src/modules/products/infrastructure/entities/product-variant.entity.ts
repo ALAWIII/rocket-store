4a917fb1b31a71f7d468650cf197eb631a33e8bf
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-23 13:36:12 +0300

    feat: add product_variants database entity with all its needed fields.

--- src/modules/products/infrastructure/entities/product-variant.entity.ts
+++ src/modules/products/infrastructure/entities/product-variant.entity.ts
@@ -1,8 +1,22 @@
 import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
-import { Entity } from 'typeorm';
+import { Column, CreateDateColumn, Entity, ForeignKey } from 'typeorm';
+import { ProductEntity } from './product.entity';
 
 @Entity('product_variants')
 export class ProductVariantEntity {
   @UuidV7PrimaryColumn()
   id!: string;
+  @Column('uuid')
+  @ForeignKey(() => ProductEntity, (p) => p.id)
+  productId!: string;
+  @Column('integer')
+  price!: number;
+  @Column('integer')
+  quantity!: number;
+  @Column('jsonb')
+  info!: Record<string, unknown>;
+  @Column('varchar', { length: 500, nullable: true })
+  description?: string | null;
+  @CreateDateColumn()
+  createdAt!: Date;
 }
