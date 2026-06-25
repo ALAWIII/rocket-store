8c48460e1ef2576cd6b732f6897b78211e43bc84
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-25 14:15:03 +0300

    feat: add payment transactions database entity.

---
+++ src/modules/payments/infrastructure/entities/payment-transaction.entity.ts
@@ -0,0 +1,50 @@
+import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ForeignKey,
+  UpdateDateColumn,
+} from 'typeorm';
+import { PaymentStatus } from '../../domain/payment-status';
+import { PaymentEntity } from './payment.entity';
+import { PaymentProviderEntity } from './payment-provider.entity';
+
+@Entity('payment_transactions')
+export class PaymentTransactionEntity {
+  @UuidV7PrimaryColumn()
+  id!: string;
+  @Column('varchar', { length: 30 })
+  status!: PaymentStatus;
+
+  @Column('uuid')
+  @ForeignKey(() => PaymentEntity, (p) => p.id)
+  paymentId!: string;
+  @Column('uuid')
+  @ForeignKey(() => PaymentProviderEntity, (p) => p.id)
+  providerId!: string;
+  @Column('text')
+  gatewayTransactionId!: string;
+  @Column('text', { nullable: true })
+  gatewayCustomerId?: string | null;
+  @Column('integer')
+  amountInMinorUnit!: number;
+
+  @Column('jsonb')
+  rawPayload!: Record<string, unknown>;
+
+  @Column('varchar', { length: 4, nullable: true })
+  cardLast4!: string | null;
+
+  @Column('varchar', { length: 30, nullable: true })
+  cardBrand!: string | null;
+
+  @Column('text', { nullable: true })
+  receiptUrl!: string | null;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
