859ae7e1575714bbf0099ac39d184b43567ea5b9
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-26 16:00:08 +0300

    feat: add promotion redemptions database entity.

---
+++ src/modules/promotions/infrastructure/entities/promotion-redemption.entity.ts
@@ -0,0 +1,28 @@
+import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
+import { Column, CreateDateColumn, Entity, ForeignKey } from 'typeorm';
+import { PromotionEntity } from './promotion.entity';
+import { OrderEntity } from 'src/modules/orders/infrastructure/entities/order.entity';
+import { CouponEntity } from './coupon.entity';
+import { UserEntity } from 'src/modules/users/infrastructure/entities/user.entity';
+
+@Entity('promotion_redemptions')
+export class PromotionRedemptionEntity {
+  @UuidV7PrimaryColumn()
+  id!: string;
+  @Column('uuid')
+  @ForeignKey(() => PromotionEntity, (p) => p.id)
+  promotionId!: string;
+  @Column('uuid')
+  @ForeignKey(() => OrderEntity, (o) => o.id)
+  orderId!: string;
+  @Column('uuid', { nullable: true })
+  @ForeignKey(() => CouponEntity, (c) => c.id)
+  couponId!: string | null;
+  @Column('uuid', { nullable: true })
+  @ForeignKey(() => UserEntity, (u) => u.id)
+  userId!: string | null;
+  @Column('integer', { default: 0 })
+  discountAmount!: number;
+  @CreateDateColumn({ type: 'timestamptz', name: 'redeemed_at' })
+  redeemedAt!: Date;
+}
