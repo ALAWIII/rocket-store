919e86bb1c4628e1e077a07f716d63e9fd3de829
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-26 20:46:41 +0300

    feat: add shipments database entity.

---
+++ src/modules/shipping/infrastructure/entities/shipment.entity.ts
@@ -0,0 +1,44 @@
+import { OrderEntity } from 'src/modules/orders/infrastructure/entities/order.entity';
+import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ForeignKey,
+  UpdateDateColumn,
+} from 'typeorm';
+import { OrderAddressEntity } from 'src/modules/users/infrastructure/entities/address.entity';
+import { ShipmentMethod } from '../../domain/shipping-method';
+import { ShipmentStatus } from '../../domain/shipping-status';
+import { ShippingProviderEntity } from './shipping-provider.entity';
+
+@Entity('shipments')
+export class ShipmentEntity {
+  @UuidV7PrimaryColumn()
+  id!: string;
+  @Column('uuid')
+  @ForeignKey(() => OrderEntity, (o) => o.id)
+  orderId!: string;
+  @Column('uuid', { nullable: true })
+  @ForeignKey(() => ShippingProviderEntity, (sp) => sp.id)
+  shippingProviderId!: string | null;
+  @Column('uuid')
+  @ForeignKey(() => OrderAddressEntity, (oa) => oa.id)
+  orderAddressId!: string;
+  @Column('varchar', { length: 20 })
+  shipmentMethod!: ShipmentMethod;
+  @Column('varchar', { length: 20 })
+  status!: ShipmentStatus;
+  @Column('integer')
+  shippingFeeInMinorUnit!: number;
+  @Column('varchar', { length: 100, nullable: true })
+  trackingNumber!: string | null;
+  @Column('timestamptz', { nullable: true })
+  shippedAt!: Date | null;
+  @Column('timestamptz', { nullable: true })
+  deliveredAt!: Date | null;
+  @CreateDateColumn()
+  createdAt!: Date;
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
