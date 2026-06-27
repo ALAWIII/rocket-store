562f38f54849ce3b3537ded47ce30602b5dfb511
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-27 21:40:50 +0300

    feat: attach serivces, providers and controllers.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { DatabaseModule } from 'src/database/database.module';
-import { DataSource } from 'typeorm';
-import { createAuth } from './auth.config';
+
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from '@thallesp/nestjs-better-auth';
-const AUTH = 'AUTH';
-const AuthProvider = {
-  provide: AUTH,
-  inject: [DataSource],
-  useFactory: (dataSource: DataSource) => {
-    return createAuth(dataSource);
-  },
-};
+import { authProvider } from './auth.provider';
+import { AuthService } from './auth.service';
+
 const GlobalGuardProvider = { provide: APP_GUARD, useClass: AuthGuard };
 @Module({
   imports: [DatabaseModule],
   controllers: [AuthController],
-  providers: [AuthProvider, GlobalGuardProvider],
+  providers: [authProvider, AuthService, GlobalGuardProvider, AuthService],
+  exports: [authProvider, AuthService],
 })
 export class AuthModule {}
