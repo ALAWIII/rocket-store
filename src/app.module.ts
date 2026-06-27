789b09b99ee685bea0af85448dc22b51f6e0b530
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-27 13:59:23 +0300

    feat: register all modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,33 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { AuditLogModule } from './modules/audit-log/audit-log.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { WishlistModule } from './modules/wishlist/wishlist.module';
+import { ShippingModule } from './modules/shipping/shipping.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
+import { PromotionsModule } from './modules/promotions/promotions.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { ImagesModule } from './modules/images/images.module';
+import { CartsModule } from './modules/carts/carts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, AuditLogModule],
+  imports: [
+    DatabaseModule,
+    AuditLogModule,
+    UsersModule,
+    ProductsModule,
+    WishlistModule,
+    ShippingModule,
+    ReviewsModule,
+    PromotionsModule,
+    PaymentsModule,
+    OrdersModule,
+    ImagesModule,
+    CartsModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
