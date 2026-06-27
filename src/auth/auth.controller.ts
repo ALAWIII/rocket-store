caf24302c4d7300f1586d4b05579139b3b7ff19e
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-27 21:40:05 +0300

    feat: implement auth controller that redirects to better auth internal router.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { All, Controller, Req, Res } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import type { Request, Response } from 'express';
+import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 
+@AllowAnonymous()
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @All('*')
+  handler(@Req() req: Request, @Res() res: Response) {
+    return this.authService.handler(req, res);
+  }
+}
