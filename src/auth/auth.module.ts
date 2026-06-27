2dbad07d159f09298e408526d980488aac825090
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-27 19:00:47 +0300

    feat: add AuthGuard globally.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,21 +3,20 @@ import { AuthController } from './auth.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { DataSource } from 'typeorm';
 import { createAuth } from './auth.config';
-export const AUTH = 'AUTH';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from '@thallesp/nestjs-better-auth';
+const AUTH = 'AUTH';
+const AuthProvider = {
+  provide: AUTH,
+  inject: [DataSource],
+  useFactory: (dataSource: DataSource) => {
+    return createAuth(dataSource);
+  },
+};
+const GlobalGuardProvider = { provide: APP_GUARD, useClass: AuthGuard };
 @Module({
   imports: [DatabaseModule],
   controllers: [AuthController],
-
-  providers: [
-    {
-      provide: AUTH,
-      inject: [DataSource],
-      useFactory: (dataSource: DataSource) => {
-        return createAuth(dataSource);
-      },
-    },
-  ],
-
-  exports: [AUTH],
+  providers: [AuthProvider, GlobalGuardProvider],
 })
 export class AuthModule {}
