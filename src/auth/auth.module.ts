9ef074b7dedf109a5dc87e9eb183c1feac5708e4
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-27 16:01:17 +0300

    feat: setup bette auth and injecting it datasource.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { DataSource } from 'typeorm';
+import { createAuth } from './auth.config';
+export const AUTH = 'AUTH';
+@Module({
+  imports: [DatabaseModule],
+  controllers: [AuthController],
 
-@Module({})
+  providers: [
+    {
+      provide: AUTH,
+      inject: [DataSource],
+      useFactory: (dataSource: DataSource) => {
+        return createAuth(dataSource);
+      },
+    },
+  ],
+
+  exports: [AUTH],
+})
 export class AuthModule {}
