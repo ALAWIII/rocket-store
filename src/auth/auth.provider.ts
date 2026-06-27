a21b865043bddf56f4845de494f861782ca130f1
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-27 20:45:18 +0300

    feat: make global auth provider that make use of injected datasource instance from initalized database.

---
+++ src/auth/auth.provider.ts
@@ -0,0 +1,12 @@
+import { FactoryProvider } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { createAuth } from './auth.config';
+
+export const AUTH = Symbol('AUTH');
+export type AuthInstance = ReturnType<typeof createAuth>;
+
+export const authProvider: FactoryProvider<AuthInstance> = {
+  provide: AUTH,
+  inject: [DataSource],
+  useFactory: (dataSource: DataSource) => createAuth(dataSource),
+};
