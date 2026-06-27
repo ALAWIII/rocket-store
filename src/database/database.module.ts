04fc46efd8703e1668c4098bf6757944b6357181
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-27 15:12:14 +0300

    feat: add data source for database configs.'

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { AppDataSource } from './data-source';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'allawiii',
-      password: '0788',
-      database: 'store',
-      autoLoadEntities: true,
-      synchronize: true,
-      namingStrategy: new SnakeNamingStrategy(),
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(AppDataSource.options)],
 })
 export class DatabaseModule {}
