290c41dbd0f86c43e1f5dd49eafcf6faeff0a08e
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-27 19:00:03 +0300

    feat: set better auth to generate uuid ids.

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -1,12 +1,14 @@
 import { betterAuth } from 'better-auth';
 import { typeormAdapter } from '@hedystia/better-auth-typeorm';
 import { DataSource } from 'typeorm';
+import { v7 } from 'uuid';
 
 export function createAuth(dataSource: DataSource) {
   return betterAuth({
     database: typeormAdapter(dataSource),
     secret: process.env.BETTER_AUTH_SECRET,
     baseURL: process.env.BETTER_AUTH_URL,
+    advanced: { database: { generateId: () => v7() } },
     emailAndPassword: {
       enabled: true,
     },
