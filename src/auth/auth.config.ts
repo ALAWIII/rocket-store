c75640ad4536e325e54257d02252f6b0e449d901
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-28 18:47:54 +0300

    feat: implement custom hashing and verifing for password using argon2.

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -2,7 +2,19 @@ import { betterAuth } from 'better-auth';
 import { typeormAdapter } from '@hedystia/better-auth-typeorm';
 import { DataSource } from 'typeorm';
 import { v7 } from 'uuid';
+import argon2 from 'argon2';
 
+async function betterHash(password: string) {
+  return argon2.hash(password, {
+    type: argon2.argon2id,
+    memoryCost: 19456,
+    timeCost: 2,
+    parallelism: 1,
+  });
+}
+async function betterVerify(data: { hash: string; password: string }) {
+  return argon2.verify(data.hash, data.password);
+}
 export function createAuth(dataSource: DataSource) {
   return betterAuth({
     database: typeormAdapter(dataSource),
@@ -11,6 +23,10 @@ export function createAuth(dataSource: DataSource) {
     advanced: { database: { generateId: () => v7() } },
     emailAndPassword: {
       enabled: true,
+      password: {
+        hash: betterHash,
+        verify: betterVerify,
+      },
     },
   });
 }
