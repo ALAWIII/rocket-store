ce57bad1cfdedad4d2a3ee3383ca66e5935884a9
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-28 12:16:46 +0300

    feat: use forRootAsync to setup the db connection.

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppDataSource } from './data-source';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import path from 'path';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(AppDataSource.options)],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        namingStrategy: new SnakeNamingStrategy(),
+        entities: [path.join(__dirname, '/../**/*.entity{.ts,.js}')],
+        autoLoadEntities: true,
+        migrationsRun: true,
+        synchronize: config.get<boolean>('DB_SYNC'), // for development only
+        poolSize: 50,
+      }),
+    }),
+  ],
   exports: [TypeOrmModule],
 })
 export class DatabaseModule {}
