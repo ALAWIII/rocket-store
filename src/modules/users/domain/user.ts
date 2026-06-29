06bb16b1ce03b0869bf48c7bb7c3bda617ba4b17
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-29 14:50:09 +0300

    feat: chopping off unneeded fields and methods.
    - add authId.
    - remove CreateUserProps, email field from UpdateUserProps and create static method.
    - update methods signature.

--- src/modules/users/domain/user.ts
+++ src/modules/users/domain/user.ts
@@ -3,83 +3,43 @@ import { Email } from 'src/modules/shared/value-objects/email';
 import { Name } from 'src/modules/shared/value-objects/name';
 import { Phone } from 'src/modules/shared/value-objects/phone';
 
-type CreateUserProps = {
-  email: string;
-  passwordHash: string;
-  firstName: string;
-  lastName: string;
-  roleId: string;
-  phone?: string;
-};
-
 type UserProps = {
   readonly id: UserId;
-  readonly createdAt: Date;
-  updatedAt: Date;
+  readonly authId: UserId;
   email: Email;
-  passwordHash: string;
   firstName: Name;
   lastName: Name;
   roleId: string;
   phone?: Phone;
+  updatedAt: Date;
+  readonly createdAt: Date;
 };
-type UpdateUserProps = {
-  email?: string;
-  passwordHash?: string;
-  firstName?: string;
-  lastName?: string;
-  roleId?: string;
-  phone?: string;
-};
-class User {
+
+type UpdateUserProps = Partial<
+  Pick<UserProps, 'firstName' | 'lastName' | 'roleId' | 'phone'>
+>;
+export class User {
   private constructor(private data: UserProps) {}
-  static create(props: CreateUserProps): User {
-    const now = new Date();
-    const email = Email.create(props.email);
-    const fName = Name.create(props.firstName);
-    const lName = Name.create(props.lastName);
-    const phone =
-      props.phone === undefined ? undefined : Phone.create(props.phone);
-    return new User({
-      id: UserId.create(),
-      passwordHash: props.passwordHash,
-      roleId: props.roleId,
-      email: email,
-      lastName: lName,
-      firstName: fName,
-      phone: phone,
-      createdAt: now,
-      updatedAt: now,
-    });
-  }
   static restore(props: UserProps) {
     return new User(props);
   }
   update(props: UpdateUserProps) {
-    if (props.email !== undefined) this.changeEmail(props.email);
-    if (props.passwordHash !== undefined)
-      this.changePasswordHash(props.passwordHash);
     if (props.firstName !== undefined) this.changeFirstName(props.firstName);
     if (props.lastName !== undefined) this.changeLastName(props.lastName);
     if (props.roleId !== undefined) this.changeRole(props.roleId);
     if (props.phone !== undefined) this.changePhone(props.phone);
     this.touch();
   }
-  private changeEmail(email: string) {
-    this.data.email = Email.create(email);
+
+  private changeFirstName(fname: Name) {
+    this.data.firstName = fname;
   }
-  private changeFirstName(fname: string) {
-    this.data.firstName = Name.create(fname);
-  }
-  private changeLastName(lastName: string) {
-    this.data.lastName = Name.create(lastName);
-  }
-  private changePasswordHash(passwordHash: string) {
-    this.data.passwordHash = passwordHash;
+  private changeLastName(lastName: Name) {
+    this.data.lastName = lastName;
   }
 
-  private changePhone(phone: string) {
-    this.data.phone = Phone.create(phone);
+  private changePhone(phone: Phone) {
+    this.data.phone = phone;
   }
 
   private changeRole(roleId: string) {
