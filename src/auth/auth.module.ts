0df0e6d5fa3eb79c48a459861e8197a1ae001e19
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-30 15:54:04 +0300

    feat: inject IUserRepository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,16 @@ import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { DatabaseModule } from 'src/database/database.module';
 import { DataSource } from 'typeorm';
 import { createAuth } from './auth.config';
+import { UsersModule } from 'src/modules/users/users.module';
+import { IUserRepository } from 'src/modules/users/infrastructure/repositories/user.repository';
 
 @Module({
   imports: [
     AuthModule.forRootAsync({
-      imports: [DatabaseModule],
-      inject: [DataSource],
-      useFactory: (dataSource: DataSource) => ({
-        auth: createAuth(dataSource),
+      imports: [DatabaseModule, UsersModule],
+      inject: [DataSource, IUserRepository],
+      useFactory: (dataSource: DataSource, userRepo: IUserRepository) => ({
+        auth: createAuth(dataSource, userRepo),
       }),
     }),
   ],
