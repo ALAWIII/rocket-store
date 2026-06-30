62c8dd5bc501292742a26e03c3c35475fc191f37
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-06-30 12:18:11 +0300

    feat: provide user repository classes.'

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './infrastructure/entities/user.entity';
+import { User as AuthUserEntity } from 'typeorm/entities/User';
+import { IUserRepository } from './infrastructure/repositories/user.repository';
+import { UserRepository } from './infrastructure/repositories/typeorm-user.repository';
+import { IAuthUserRepository } from './infrastructure/repositories/auth-user.repository';
+import { AuthUserRepository } from './infrastructure/repositories/typeorm-auth-user.repository';
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity, AuthUserEntity])],
+  providers: [
+    { provide: IUserRepository, useClass: UserRepository },
+    { provide: IAuthUserRepository, useClass: AuthUserRepository },
+  ],
+})
 export class UsersModule {}
