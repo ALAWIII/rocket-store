1822c3f3bf49f12076470568d02e92d651dc6ced
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-01 20:27:04 +0300

    feat: change name length to 50 character, remove roleType, add permission fields with PermissionJson type.

--- src/modules/access-control/infrastructure/entities/role.entity.ts
+++ src/modules/access-control/infrastructure/entities/role.entity.ts
@@ -1,13 +1,19 @@
 import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
 import { Entity, Column } from 'typeorm';
 
+type PermissionJson = {
+  entity: string;
+  action: string;
+  scope: string;
+};
+
 @Entity('roles')
 export class RoleEntity {
   @UuidV7PrimaryColumn()
   id!: string;
-  @Column({ type: 'varchar', length: 20, unique: true })
+  @Column({ type: 'varchar', length: 50, unique: true })
   name!: string;
 
-  @Column({ type: 'varchar', length: 10 })
-  roleType!: string;
+  @Column({ type: 'jsonb', default: () => "'[]'" })
+  permissions!: PermissionJson[];
 }
