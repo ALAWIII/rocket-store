6c50f939c7a003d362b90892f348def5e39ea710
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-03 19:37:40 +0300

    feat: provide address repositories.

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -6,12 +6,18 @@ import { IUserRepository } from './infrastructure/repositories/user.repository';
 import { UserRepository } from './infrastructure/repositories/typeorm-user.repository';
 import { IAuthUserRepository } from './infrastructure/repositories/auth-user.repository';
 import { AuthUserRepository } from './infrastructure/repositories/typeorm-auth-user.repository';
+import { IAddressRepository } from './infrastructure/repositories/address.repository';
+import { AddressRepository } from './infrastructure/repositories/typeorm-address.repository';
+import { IOrderAddressRepository } from './infrastructure/repositories/order-address.repository';
+import { OrderAddressRepositroy } from './infrastructure/repositories/typeorm-order-address.repository';
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity, AuthUserEntity])],
   providers: [
     { provide: IUserRepository, useClass: UserRepository },
     { provide: IAuthUserRepository, useClass: AuthUserRepository },
+    { provide: IAddressRepository, useClass: AddressRepository },
+    { provide: IOrderAddressRepository, useClass: OrderAddressRepositroy },
   ],
-  exports: [IUserRepository],
+  exports: [IUserRepository, IAddressRepository, IOrderAddressRepository],
 })
 export class UsersModule {}
