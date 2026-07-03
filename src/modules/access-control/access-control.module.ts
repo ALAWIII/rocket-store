64ab1ca013bb00c9dfcff7de79c0c018ce48a0f8
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-03 19:52:10 +0300

    feat: import repositories and entities.

--- src/modules/access-control/access-control.module.ts
+++ src/modules/access-control/access-control.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { IRoleRepository } from './infrastructure/repositories/role.repository';
+import { RoleRepository } from './infrastructure/repositories/typeorm-role.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleEntity } from './infrastructure/entities/role.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([RoleEntity])],
+  providers: [{ provide: IRoleRepository, useClass: RoleRepository }],
+})
 export class AccessControlModule {}
