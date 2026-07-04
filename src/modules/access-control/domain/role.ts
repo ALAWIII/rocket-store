96d6a370414dd948efbf1d9c604b26eb6629cda9
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-04 15:41:28 +0300

    feat: add permissions getter.

--- src/modules/access-control/domain/role.ts
+++ src/modules/access-control/domain/role.ts
@@ -6,21 +6,21 @@ export class Role {
   constructor(
     private readonly _id: RoleId,
     private _name: Name, // unique
-    private permissions: Permission[],
+    private _permissions: Permission[],
   ) {}
   findPermission(perm: Permission): number {
-    return this.permissions.findIndex((p) => p.equals(perm));
+    return this._permissions.findIndex((p) => p.equals(perm));
   }
   addPermission(perm: Permission) {
     const index = this.findPermission(perm);
     if (index >= 0) return;
 
-    this.permissions.push(perm);
+    this._permissions.push(perm);
   }
   removePermission(perm: Permission) {
     const index = this.findPermission(perm);
     if (index < 0) return;
-    this.permissions.splice(index, 1);
+    this._permissions.splice(index, 1);
   }
   setName(name: string) {
     this._name = Name.create(name);
@@ -31,11 +31,14 @@ export class Role {
   get id(): string {
     return this._id.toString();
   }
+  get permissions(): Permission[] {
+    return this._permissions;
+  }
   toJSON() {
     return {
       id: this._id,
       name: this._name,
-      permissions: this.permissions.map((p) => p),
+      permissions: this._permissions.map((p) => p),
     };
   }
 }
