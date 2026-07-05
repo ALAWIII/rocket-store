533b96abc907951b26810f5aa8f6911b74d5866f
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-05 19:17:34 +0300

    feat: provide and import modules and setup AuthZModule.

--- src/modules/access-control/access-control.module.ts
+++ src/modules/access-control/access-control.module.ts
@@ -3,9 +3,38 @@ import { IRoleRepository } from './infrastructure/repositories/role.repository';
 import { RoleRepository } from './infrastructure/repositories/typeorm-role.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoleEntity } from './infrastructure/entities/role.entity';
-
+import { AccessControlBootstrapService } from './application/access-control.bootstrap.service';
+import { AccessControlSyncService } from './application/access-control-sync.service';
+import { AccessControlService } from './access-control.service';
+import { SystemRolesRegistry } from './application/system-roles.registry';
+import { AUTHZ_ENFORCER, AuthZModule } from 'nest-authz';
+import { createCasbinEnforcer } from './infrastructure/casbin/casbin.factory';
+import { AuthenticatedRequest } from 'src/auth/auth.config';
 @Module({
-  imports: [TypeOrmModule.forFeature([RoleEntity])],
-  providers: [{ provide: IRoleRepository, useClass: RoleRepository }],
+  imports: [
+    TypeOrmModule.forFeature([RoleEntity]),
+    AuthZModule.register({
+      enforcerProvider: {
+        provide: AUTHZ_ENFORCER,
+        useFactory: async () => createCasbinEnforcer(),
+      },
+      userFromContext: (ctx) => {
+        const request = ctx.switchToHttp().getRequest<AuthenticatedRequest>();
+        return request.user.roleId ?? null;
+      },
+    }),
+  ],
+  providers: [
+    AccessControlBootstrapService,
+    AccessControlSyncService,
+    AccessControlService,
+    SystemRolesRegistry,
+    { provide: IRoleRepository, useClass: RoleRepository },
+  ],
+  exports: [
+    AccessControlService,
+    AccessControlSyncService,
+    SystemRolesRegistry,
+  ],
 })
 export class AccessControlModule {}
