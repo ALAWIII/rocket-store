cd091697276ce682d13a815e876344010d614203
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-05 11:53:33 +0300

    feat: remove auth user repository.

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './infrastructure/entities/user.entity';
-import { User as AuthUserEntity } from 'typeorm/entities/User';
 import { IUserRepository } from './infrastructure/repositories/user.repository';
 import { UserRepository } from './infrastructure/repositories/typeorm-user.repository';
-import { IAuthUserRepository } from './infrastructure/repositories/auth-user.repository';
-import { AuthUserRepository } from './infrastructure/repositories/typeorm-auth-user.repository';
 import { IAddressRepository } from './infrastructure/repositories/address.repository';
 import { AddressRepository } from './infrastructure/repositories/typeorm-address.repository';
 import { IOrderAddressRepository } from './infrastructure/repositories/order-address.repository';
@@ -16,16 +13,10 @@ import {
 } from './infrastructure/entities/address.entity';
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      UserEntity,
-      AuthUserEntity,
-      AddressEntity,
-      OrderAddressEntity,
-    ]),
+    TypeOrmModule.forFeature([UserEntity, AddressEntity, OrderAddressEntity]),
   ],
   providers: [
     { provide: IUserRepository, useClass: UserRepository },
-    { provide: IAuthUserRepository, useClass: AuthUserRepository },
     { provide: IAddressRepository, useClass: AddressRepository },
     { provide: IOrderAddressRepository, useClass: OrderAddressRepositroy },
   ],
