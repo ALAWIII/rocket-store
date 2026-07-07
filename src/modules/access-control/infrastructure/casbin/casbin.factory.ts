63cc8fbd59a911443f2d26e916d1128f6159b761
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-07 14:15:52 +0300

    fix: assets distination resolution issue.

--- src/modules/access-control/infrastructure/casbin/casbin.factory.ts
+++ src/modules/access-control/infrastructure/casbin/casbin.factory.ts
@@ -1,10 +1,8 @@
-// src/modules/access-control/infrastructure/casbin/casbin.factory.ts
-import { join } from 'node:path';
+import path from 'node:path';
 import { newEnforcer, Enforcer } from 'casbin';
 
 export async function createCasbinEnforcer(): Promise<Enforcer> {
-  const modelPath = join(__dirname, 'model.conf');
-
+  const modelPath = path.join(__dirname, 'model.conf');
   const enforcer = await newEnforcer(modelPath);
 
   enforcer.clearPolicy();
