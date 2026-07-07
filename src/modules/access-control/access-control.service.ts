61a0d19ce6fe524ea1a35e4d670c22bcfdc5134e
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-07 19:23:10 +0300

    feat: add removeRole service method.

--- src/modules/access-control/access-control.service.ts
+++ src/modules/access-control/access-control.service.ts
@@ -3,12 +3,16 @@ import { IRoleRepository } from './infrastructure/repositories/role.repository';
 import { AccessControlSyncService } from './application/access-control-sync.service';
 import { Permission } from './domain/permission';
 import { CreateRoleDto } from './dto/create-role.dto';
+import { IUserRepository } from '../users/infrastructure/repositories/user.repository';
+import { SystemRolesRegistry } from './application/system-roles.registry';
 
 @Injectable()
 export class AccessControlService {
   constructor(
     private readonly roleRepo: IRoleRepository,
     private readonly acsyncSerivce: AccessControlSyncService,
+    private readonly userRepo: IUserRepository,
+    private readonly systemRole: SystemRolesRegistry,
   ) {}
   async createRole(roleData: CreateRoleDto): Promise<string | null> {
     const perms = roleData.permissions.map((p) =>
@@ -19,5 +23,15 @@ export class AccessControlService {
       await this.acsyncSerivce.upsertRole(role);
     }
     return role?.id ?? null;
+  }
+  async removeRole(roleId: string) {
+    const isSystemRole = this.systemRole.hasId(roleId);
+    if (isSystemRole) throw new Error('System roles cannot be removed');
+    await this.userRepo.reassignUsersRole(
+      roleId,
+      this.systemRole.getCustomerRoleId(),
+    );
+    await this.acsyncSerivce.removeRole(roleId);
+    await this.roleRepo.removeById(roleId);
   }
 }
