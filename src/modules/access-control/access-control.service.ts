af673d9a76fbb40d563f0b5b157b440cc8699976
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-09 21:11:49 +0300

    feat: change return type from id to full role object.

--- src/modules/access-control/access-control.service.ts
+++ src/modules/access-control/access-control.service.ts
@@ -5,6 +5,7 @@ import { Permission } from './domain/permission';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { IUserRepository } from '../users/infrastructure/repositories/user.repository';
 import { SystemRolesRegistry } from './application/system-roles.registry';
+import { RoleResponseDto } from './dto/role-response.dto';
 
 @Injectable()
 export class AccessControlService {
@@ -14,7 +15,7 @@ export class AccessControlService {
     private readonly systemRole: SystemRolesRegistry,
     private readonly acsyncService: AccessControlSyncService,
   ) {}
-  async createRole(roleData: CreateRoleDto): Promise<string | null> {
+  async upsertRole(roleData: CreateRoleDto): Promise<RoleResponseDto | null> {
     if (this.systemRole.isSystemRoleName(roleData.name)) return null; // the null means, cant modify system Role
 
     const perms = roleData.permissions.map((p) =>
@@ -24,7 +25,11 @@ export class AccessControlService {
     const role = await this.roleRepo.upsertByName(roleData.name, perms);
     await this.acsyncService.upsertRole(role);
 
-    return role.id;
+    return {
+      id: role.id,
+      name: role.name,
+      permissions: role.permissions.map((p) => p.toJSON()),
+    };
   }
   async removeRole(roleId: string): Promise<number> {
     const isSystemRole = this.systemRole.hasId(roleId);
