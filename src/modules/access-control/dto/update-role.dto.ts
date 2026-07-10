f8a910ed4c819bade64ddfb49bdae13c1f4f06c4
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-10 18:12:48 +0300

    feat: make UpdateRoleDto fields required.

--- src/modules/access-control/dto/update-role.dto.ts
+++ src/modules/access-control/dto/update-role.dto.ts
@@ -1,26 +1,13 @@
-import { AtLeastOneDefined } from 'src/modules/shared/validation/decorators/at-least-one-defined.decorator';
 import { PermissionDto } from './permission.dto';
-import {
-  ArrayNotEmpty,
-  IsArray,
-  IsOptional,
-  Length,
-  ValidateNested,
-} from 'class-validator';
+import { IsArray, Length, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class UpdateRoleDto {
-  @IsOptional()
   @Length(2, 50)
-  name?: string;
+  name!: string;
 
-  @IsOptional()
   @IsArray()
-  @ArrayNotEmpty()
   @ValidateNested({ each: true })
   @Type(() => PermissionDto)
-  permissions?: PermissionDto[];
-
-  @AtLeastOneDefined(['name', 'permissions'])
-  private readonly __atLeastOne?: never;
+  permissions!: PermissionDto[];
 }
