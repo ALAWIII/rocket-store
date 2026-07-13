e838d9bb52b7fc684d643ced410c1921a902bcd0
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-13 20:47:20 +0300

    feat: replace InvalidPermissionEntityError with its base class.

--- src/error/error-mapping.bootstrap.service.ts
+++ src/error/error-mapping.bootstrap.service.ts
@@ -11,7 +11,7 @@ import {
   RecordNotFoundError,
   UniqueViolationError,
 } from 'src/modules/shared/errors/database.error';
-import { InvalidPermissionEntityError } from 'src/modules/access-control/domain/permission.error';
+import { PermissionError } from 'src/modules/access-control/domain/permission.error';
 import { InvalidValueObjectError } from 'src/modules/shared/value-objects/value-object.error';
 import { SystemRoleError } from 'src/modules/access-control/application/system-roles.error';
 
@@ -22,10 +22,7 @@ export class ErrorMappingBootstrap implements OnModuleInit {
     this.registry
       .register(RecordNotFoundError, (e) => new NotFoundException(e.message))
       .register(UniqueViolationError, (e) => new ConflictException(e.message))
-      .register(
-        InvalidPermissionEntityError,
-        (e) => new BadRequestException(e.message),
-      )
+      .register(PermissionError, (e) => new BadRequestException(e.message))
       .register(
         InvalidValueObjectError,
         (e) => new UnprocessableEntityException(e.message),
