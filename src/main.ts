7ced8e65ed81010eac7e5e1776e71d80fcd179e4
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-14 20:16:23 +0300

    feat: add global prefix with api version 1.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'reflect-metadata';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false });
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
