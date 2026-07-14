7f5cb05fccc388f4d9a686502f9a9ecea13cfc2d
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-14 15:19:27 +0300

    feat: update IsValidPermissionConstraint logic.
    - add hasCompletePermissionDto, PermissionDto, CompletePermissionDto.
    - use Permission.fromPrimitives.

--- src/modules/shared/validation/constraints/is-valid-permission.constraint.ts
+++ src/modules/shared/validation/constraints/is-valid-permission.constraint.ts
@@ -5,25 +5,26 @@ import {
 } from 'class-validator';
 import { Permission } from 'src/modules/access-control/domain/permission';
 
+type PermissionDto = {
+  entity?: string;
+  action?: string;
+  scope?: string;
+};
+function hasCompletePermissionDto(
+  dto: PermissionDto,
+): dto is CompletePermissionDto {
+  return !!dto.entity && !!dto.action && !!dto.scope;
+}
+
+type CompletePermissionDto = Required<PermissionDto>;
 @ValidatorConstraint({ name: 'isValidPermission', async: false })
 export class IsValidPermissionConstraint implements ValidatorConstraintInterface {
   validate(_: unknown, args: ValidationArguments): boolean {
-    const dto = args.object as {
-      entity?: string;
-      action?: string;
-      scope?: string;
-    };
-
-    if (!dto.entity || !dto.action || !dto.scope) {
+    const dto = args.object as PermissionDto;
+    if (!hasCompletePermissionDto(dto)) {
       return false;
     }
-
-    try {
-      Permission.fromString(`${dto.entity}.${dto.action}.${dto.scope}`);
-      return true;
-    } catch {
-      return false;
-    }
+    return Permission.fromPrimitives(dto).isOk();
   }
 
   defaultMessage(): string {
