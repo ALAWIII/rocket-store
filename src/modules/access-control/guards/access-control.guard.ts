d5b05abc0bd88a912d2487e4c1a33bbdf73a0565
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-15 16:25:09 +0300

    feat: inject IEnforcerHolder and use its enforce method.

--- src/modules/access-control/guards/access-control.guard.ts
+++ src/modules/access-control/guards/access-control.guard.ts
@@ -5,23 +5,19 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Enforcer } from 'casbin';
-import {
-  AUTHZ_ENFORCER,
-  AUTHZ_MODULE_OPTIONS,
-  type AuthZModuleOptions,
-} from 'nest-authz';
+import { AUTHZ_MODULE_OPTIONS, type AuthZModuleOptions } from 'nest-authz';
 import {
   PERMISSION_KEY,
   PermissionAttr,
 } from 'src/modules/shared/authorization/decorators/require-permission.decorator';
 import { AppUser } from 'src/auth/auth.config';
+import { IEnforcerHolder } from '../infrastructure/casbin/enforcer-holder';
 
 @Injectable()
 export class AccessGuard implements CanActivate {
   constructor(
     private readonly reflector: Reflector,
-    @Inject(AUTHZ_ENFORCER) private readonly enforcer: Enforcer,
+    @Inject(IEnforcerHolder) private readonly enforcerHolder: IEnforcerHolder,
     @Inject(AUTHZ_MODULE_OPTIONS)
     private readonly options: AuthZModuleOptions,
   ) {}
@@ -34,11 +30,11 @@ export class AccessGuard implements CanActivate {
     if (!permission) return true;
 
     const user = this.options.userFromContext(context) as AppUser;
-    return await this.enforcer.enforce(
-      user.roleId,
-      permission.entity,
-      permission.action,
-      permission.scope,
-    );
+    return await this.enforcerHolder.enforce({
+      roleId: user.roleId,
+      entity: permission.entity,
+      action: permission.action,
+      scope: permission.scope,
+    });
   }
 }
