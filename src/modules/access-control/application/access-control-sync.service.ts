68e37a4c5a3eaabf68ac84f493cf3a1f22faf922
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-15 14:34:51 +0300

    feat: update sync service methods.
    - replace direct AUTHZ_ENFORCER injection with IEnforcerHolder custom holder.
    - reloadFromDatabase: replace clearPolicy method call with creating new enforcer instance and setting/feeding it into the EnforcerHolder.
    - getPermissions: replace getFilteredPolicy with a specific wrapped one getPoliciesById.

--- src/modules/access-control/application/access-control-sync.service.ts
+++ src/modules/access-control/application/access-control-sync.service.ts
@@ -1,18 +1,18 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
-import type { Enforcer } from 'casbin';
 
 import { IRoleRepository } from '../infrastructure/repositories/role.repository';
 import type { Role } from '../domain/role';
-import { AUTHZ_ENFORCER } from 'nest-authz';
 import { Permission } from '../domain/permission';
+import { createCasbinEnforcer } from '../infrastructure/casbin/casbin.factory';
+import { IEnforcerHolder } from '../infrastructure/casbin/enforcer-holder';
 
 @Injectable()
 export class AccessControlSyncService {
   private readonly logger = new Logger(AccessControlSyncService.name);
 
   constructor(
-    @Inject(AUTHZ_ENFORCER)
-    private readonly enforcer: Enforcer,
+    @Inject(IEnforcerHolder)
+    private readonly enforcer: IEnforcerHolder,
     @Inject(IRoleRepository)
     private readonly roleRepository: IRoleRepository,
   ) {}
@@ -23,26 +23,27 @@ export class AccessControlSyncService {
       throw roles.error;
     }
 
-    const policies = this.toPolicies(roles.unwrap());
-
-    this.enforcer.clearPolicy();
+    const allRoles = roles.unwrap();
+    const policies = this.toPolicies(allRoles);
+    const newEnforcer = await createCasbinEnforcer();
 
     if (policies.length > 0) {
-      await this.enforcer.addPolicies(policies);
+      await newEnforcer.addPolicies(policies);
     }
 
+    this.enforcer.set(newEnforcer);
+
     this.logger.log(
-      `Casbin policies reloaded: ${policies.length} policies from ${roles.unwrap().length} roles`,
+      `Casbin policies reloaded: ${policies.length} policies from ${allRoles.length} roles`,
     );
   }
   async getPermissions(roleId: string): Promise<Map<string, Permission>> {
-    const policies = (await this.enforcer.getFilteredPolicy(0, roleId)).map(
-      (p) =>
-        Permission.fromPrimitives({
-          entity: p[1],
-          action: p[2],
-          scope: p[3],
-        }).unwrap(),
+    const policies = (await this.enforcer.getPoliciesById(roleId)).map((p) =>
+      Permission.fromPrimitives({
+        entity: p[1],
+        action: p[2],
+        scope: p[3],
+      }).unwrap(),
     );
     const permissionMap = new Map(policies.map((p) => [p.key(), p]));
     return permissionMap;
@@ -57,7 +58,7 @@ export class AccessControlSyncService {
     }
   }
   async removeRole(roleId: string): Promise<boolean> {
-    const currentPolicies = await this.enforcer.getFilteredPolicy(0, roleId);
+    const currentPolicies = await this.enforcer.getPoliciesById(roleId);
 
     if (currentPolicies.length === 0) return true;
 
@@ -69,7 +70,7 @@ export class AccessControlSyncService {
     return this.enforcer.addPolicies(policies);
   }
   async hasRole(roleId: string): Promise<boolean> {
-    const policies = await this.enforcer.getFilteredPolicy(0, roleId);
+    const policies = await this.enforcer.getPoliciesById(roleId);
     return policies.length > 0;
   }
   private toPolicies(roles: Role[]): string[][] {
