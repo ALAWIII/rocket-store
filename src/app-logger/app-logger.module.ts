da24804352a3e002a2b5b41f14bca07268b5b108
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-16 20:51:13 +0300

    feat: continue setting up the logger.
    - implement genReqId to generate and return uuid v7 id.
    - attach options like pino-pretty that only used in development environment.

--- src/app-logger/app-logger.module.ts
+++ src/app-logger/app-logger.module.ts
@@ -1,16 +1,40 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
+import { v7 } from 'uuid';
 
+const DEVELOPMENT_OPTIONS = {
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      singleLine: true,
+      colorize: true,
+      translateTime: 'SYS:standard',
+    },
+  },
+  autoLogging: true, // enabled by default for both, but put here to ensure that its required in development environment.
+};
 @Module({
   imports: [
     LoggerModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
+        const isDev = config.get<string>('NODE_ENV') === 'production';
         return {
           pinoHttp: {
-            level: config.get<string>('DEVELOPMENT_ENV') ? 'debug' : 'info',
+            level: config.get<string>('LOG_LEVEL') ?? 'info',
+            genReqId: (req, res) => {
+              const inReqId = req.headers['x-request-id'];
+              const reqId =
+                typeof inReqId === 'string' && inReqId.trim() !== ''
+                  ? inReqId
+                  : undefined;
+              const id = v7();
+              res.setHeader('x-request-id', reqId ?? id);
+              return id;
+            },
+            ...(isDev ? DEVELOPMENT_OPTIONS : undefined),
           },
         };
       },
