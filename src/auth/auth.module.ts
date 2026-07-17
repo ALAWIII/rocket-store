e0fe56e9301df7b076de667033ec2b8a0ed8bf03
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-17 18:58:52 +0300

    feat: accept log level and pass it down to createAuth, inject Logger and ConfigService.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,23 @@ import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { DatabaseModule } from 'src/database/database.module';
 import { DataSource } from 'typeorm';
 import { createAuth } from './auth.config';
+import { ConfigService } from '@nestjs/config';
+import { AppLogLevel, toAppLogLevel } from 'src/app-logger/app-log.level';
+import { Logger } from 'nestjs-pino';
 
 @Module({
   imports: [
     AuthModule.forRootAsync({
       imports: [DatabaseModule],
-      inject: [DataSource],
-      useFactory: (dataSource: DataSource) => ({
-        auth: createAuth(dataSource),
-      }),
+      inject: [DataSource, Logger, ConfigService],
+      useFactory: (
+        dataSource: DataSource,
+        logger: Logger,
+        config: ConfigService,
+      ) => {
+        const logLevel: AppLogLevel = toAppLogLevel(config.get('LOG_LEVEL'));
+        return { auth: createAuth(dataSource, logger, logLevel) };
+      },
     }),
   ],
 })
