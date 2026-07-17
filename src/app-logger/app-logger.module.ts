5ea1114faf95a8bd4f3f4a564ab4f9433048926b
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-17 13:50:16 +0300

    feat: update logging settings.
    - remove for specific development options.
    - remove the need for pino-pretty as file logging is enough.
    - use pino-roll with hourly emitting to files.

--- src/app-logger/app-logger.module.ts
+++ src/app-logger/app-logger.module.ts
@@ -1,40 +1,48 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
+import path from 'node:path';
 import { v7 } from 'uuid';
 
-const DEVELOPMENT_OPTIONS = {
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      singleLine: true,
-      colorize: true,
-      translateTime: 'SYS:standard',
-    },
-  },
-  autoLogging: true, // enabled by default for both, but put here to ensure that its required in development environment.
-};
 @Module({
   imports: [
     LoggerModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
-        const isDev = config.get('NODE_ENV') === 'development';
+        const level = config.get<string>('LOG_LEVEL') ?? 'info';
+
         return {
           pinoHttp: {
-            level: config.get<string>('LOG_LEVEL') ?? 'info',
+            level,
+            autoLogging: true,
             genReqId: (req, res) => {
               const inReqId = req.headers['x-request-id'];
               const reqId =
                 typeof inReqId === 'string' && inReqId.trim() !== ''
                   ? inReqId
                   : undefined;
+
               const id = v7();
               res.setHeader('x-request-id', reqId ?? id);
               return id;
             },
-            ...(isDev ? DEVELOPMENT_OPTIONS : undefined),
+            transport: {
+              targets: [
+                {
+                  level,
+                  target: 'pino-roll',
+                  options: {
+                    file: path.join('logs', 'app'),
+                    frequency: 'hourly',
+                    dateFormat: 'yyyy-MM-dd-HH',
+                    extension: '.jsonl',
+                    mkdir: true,
+                    limit: { count: 168 }, // 7 days
+                  },
+                },
+              ],
+            },
           },
         };
       },
