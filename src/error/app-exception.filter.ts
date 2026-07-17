b56aa4e6feddec8e7dc275978f36000625c90a9f
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-17 14:34:20 +0300

    feat(logger): add structured exception logging and error mapping
    - logException private method.
    - re-arrange catch method code execution.

--- src/error/app-exception.filter.ts
+++ src/error/app-exception.filter.ts
@@ -3,21 +3,42 @@ import {
   Catch,
   ExceptionFilter,
   InternalServerErrorException,
+  Logger,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { ErrorMapperRegistry } from './error-mapper.registry';
+import { isLoggableError } from 'src/modules/shared/errors/error.types';
 
 @Catch()
 export class AppExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AppExceptionFilter.name);
   constructor(private readonly registry: ErrorMapperRegistry) {}
 
   catch(exception: unknown, host: ArgumentsHost) {
-    const res = host.switchToHttp().getResponse<Response>();
+    this.logException(exception);
+
     const httpException =
       exception instanceof Error
         ? this.registry.map(exception)
         : new InternalServerErrorException('Internal server error');
 
+    const res = host.switchToHttp().getResponse<Response>();
     res.status(httpException.getStatus()).json(httpException.getResponse());
+  }
+  private logException(exception: unknown) {
+    if (isLoggableError(exception)) {
+      this.logger.error(exception.toLog());
+      return;
+    }
+    if (exception instanceof Error) {
+      this.logger.error({
+        name: exception.name,
+        message: exception.message,
+        stack: exception.stack,
+      });
+      return;
+    }
+
+    this.logger.error({ message: 'Non-Error exception thrown', exception });
   }
 }
