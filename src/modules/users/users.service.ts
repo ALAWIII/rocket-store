1b5b90179df77a5a5c60c865bd9817800ec76ff7
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-26 16:48:18 +0300

    feat: add findAll, findUser user service methods.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -4,5 +4,12 @@ import { IUserRepository } from './infrastructure/repositories/user.repository';
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepo: IUserRepository) {}
-  findAll() {}
+  async findAll(d: { requesterRoleId: string; page?: number; limit?: number }) {
+    const result = (await this.userRepo.findAll(d)).unwrap();
+    return { users: result.users.map((u) => u.toJSON()), total: result.total };
+  }
+  async findUser(id: string) {
+    const user = (await this.userRepo.findUserRequester(id)).unwrap();
+    return user.toJSON();
+  }
 }
