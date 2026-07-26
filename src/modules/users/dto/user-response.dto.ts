b9f0bdf8f8cee6b29369a955949dfd9b78ed2c15
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-26 20:17:36 +0300

    feat: add UserResponseDto.

---
+++ src/modules/users/dto/user-response.dto.ts
@@ -0,0 +1,12 @@
+export type UserResponseDto = {
+  id: string;
+  email: string;
+  name: string;
+  givenName: string;
+  familyName: string;
+  roleId: string;
+  image?: string;
+  phone?: string;
+  updatedAt: Date;
+  createdAt: Date;
+};
