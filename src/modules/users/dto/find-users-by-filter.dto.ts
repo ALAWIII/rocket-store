998578dbcd8d11d73b7887a5c2fc00f0edac83c9
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-26 17:21:55 +0300

    feat: add FindUsersByParamsDto, UserFiltersDto dto

---
+++ src/modules/users/dto/find-users-by-filter.dto.ts
@@ -0,0 +1,50 @@
+import { Type } from 'class-transformer';
+import {
+  IsOptional,
+  IsString,
+  Length,
+  ValidateNested,
+  IsInt,
+  Min,
+  IsEmail,
+  IsUUID,
+} from 'class-validator';
+
+class UserFiltersDto {
+  @IsOptional()
+  @IsString()
+  @Length(2, 50)
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsUUID('7')
+  roleId?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(2, 20)
+  phone?: string;
+}
+
+export class FindUsersByParamsDto {
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => UserFiltersDto)
+  filters?: UserFiltersDto;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  limit?: number;
+}
