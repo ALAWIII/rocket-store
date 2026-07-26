c78de3a16b9541b5d7ffb218eae8b730d025e9e0
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-26 17:34:07 +0300

    feat: add FindAllUsersDto and update findAll input signature accordingly.

---
+++ src/modules/users/dto/find-all-users.dto.ts
@@ -0,0 +1,15 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Min } from 'class-validator';
+
+export class FindAllUsersDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number;
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  limit?: number;
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { IUserRepository } from './infrastructure/repositories/user.repository';
 import { FindUsersByParamsDto } from './dto/find-users-by-filter.dto';
+import { FindAllUsersDto } from './dto/find-all-users.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepo: IUserRepository) {}
-  async findAll(d: { requesterRoleId: string; page?: number; limit?: number }) {
-    const result = (await this.userRepo.findAll(d)).unwrap();
+  async findAll(requesterRoleId: string, d: FindAllUsersDto) {
+    const result = (
+      await this.userRepo.findAll({ ...d, requesterRoleId })
+    ).unwrap();
     return { users: result.users.map((u) => u.toJSON()), total: result.total };
   }
   async findUser(id: string) {
