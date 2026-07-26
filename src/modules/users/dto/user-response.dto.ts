6aeca416bdc25e9a63335063fecae975b89e9bea
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-26 20:33:23 +0300

    refactor(dto): change UserResponseDto from plain type to class dto.

--- src/modules/users/dto/user-response.dto.ts
+++ src/modules/users/dto/user-response.dto.ts
@@ -1,12 +1,12 @@
-export type UserResponseDto = {
-  id: string;
-  email: string;
-  name: string;
-  givenName: string;
-  familyName: string;
-  roleId: string;
+export class UserResponseDto {
+  id!: string;
+  email!: string;
+  name!: string;
+  givenName!: string;
+  familyName!: string;
+  roleId!: string;
   image?: string;
   phone?: string;
-  updatedAt: Date;
-  createdAt: Date;
-};
+  updatedAt!: Date;
+  createdAt!: Date;
+}
