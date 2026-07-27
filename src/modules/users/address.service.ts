e7f3c74f748ae10efbe4b2f716bdb4f796964f34
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-27 19:49:58 +0300

    feat: add updateAdrs service method.

--- src/modules/users/address.service.ts
+++ src/modules/users/address.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { IAddressRepository } from './infrastructure/repositories/address.repository';
+import { UpdateAddressDto } from './dto/update-address.dto';
+import { Address } from './domain/address';
 
 @Injectable()
 export class AddressService {
@@ -14,5 +16,15 @@ export class AddressService {
       throw new NotFoundException(`Address ${adrsId} not found.`);
 
     return address.unwrap();
+  }
+  async updateAdrs(id: string, userId: string, data: UpdateAddressDto) {
+    const adrs = Address.fromPrimitives({
+      ...data,
+      id,
+      userId,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }).unwrap();
+    return (await this.addressRepo.update(adrs)).unwrap();
   }
 }
