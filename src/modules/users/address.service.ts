a9553195c2e2e2567cb6036d6ff92d1f8ea56496
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-27 20:38:51 +0300

    refactor: attach the return type for every service method and use toPrimitives.

--- src/modules/users/address.service.ts
+++ src/modules/users/address.service.ts
@@ -4,22 +4,27 @@ import { UpdateAddressDto } from './dto/update-address.dto';
 import { Address } from './domain/address';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { AddressId } from '../shared/domain/ids';
+import { AddressResponseDto } from './dto/address-response.dto';
 
 @Injectable()
 export class AddressService {
   constructor(private readonly addressRepo: IAddressRepository) {}
-  async findAll(userId: string) {
+
+  async findAll(userId: string): Promise<AddressResponseDto[]> {
     const addresses = (await this.addressRepo.findAll(userId)).unwrap();
     return addresses.map((ad) => ad.toPrimitives());
   }
-  async findById(adrsId: string) {
+  async findById(adrsId: string): Promise<AddressResponseDto> {
     const address = (await this.addressRepo.findById(adrsId)).unwrap();
     if (address.isNone())
       throw new NotFoundException(`Address ${adrsId} not found.`);
 
-    return address.unwrap();
+    return address.unwrap().toPrimitives();
   }
-  async createAdrs(userId: string, data: CreateAddressDto) {
+  async createAdrs(
+    userId: string,
+    data: CreateAddressDto,
+  ): Promise<AddressResponseDto> {
     const newDate = new Date();
     const newAdrs = Address.fromPrimitives({
       id: AddressId.create().toString(),
@@ -30,7 +35,11 @@ export class AddressService {
     }).unwrap();
     return (await this.addressRepo.create(newAdrs)).unwrap().toPrimitives();
   }
-  async updateAdrs(id: string, userId: string, data: UpdateAddressDto) {
+  async updateAdrs(
+    id: string,
+    userId: string,
+    data: UpdateAddressDto,
+  ): Promise<AddressResponseDto> {
     const adrs = Address.fromPrimitives({
       ...data,
       id,
@@ -38,6 +47,6 @@ export class AddressService {
       createdAt: new Date(),
       updatedAt: new Date(),
     }).unwrap();
-    return (await this.addressRepo.update(adrs)).unwrap();
+    return (await this.addressRepo.update(adrs)).unwrap().toPrimitives();
   }
 }
