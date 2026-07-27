5c7d59ba35c245648eb57a41778ef062aa8b6854
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-27 21:17:06 +0300

    feat: add findAll,findById controller endpoints methods handlers and two versions, one for user own, and the other for adminstrative.

---
+++ src/modules/users/addresses.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  UseGuards,
+} from '@nestjs/common';
+import { AccessGuard } from '../access-control/guards/access-control.guard';
+import { AddressService } from './address.service';
+import { Session } from '@thallesp/nestjs-better-auth';
+import { type AppSession } from 'src/auth/auth.config';
+import { RequirePermission } from '../shared/authorization/decorators/require-permission.decorator';
+import { AllPermissions } from '../access-control/domain/permission';
+
+@Controller('me/addresses')
+export class MyAddressesController {
+  constructor(private readonly service: AddressService) {}
+
+  @Get()
+  async findAll(@Session() session: AppSession) {
+    return await this.service.findAll(session.user.id);
+  }
+  @Get(':id')
+  async findById(
+    @Param('id', new ParseUUIDPipe({ version: '7' })) id: string,
+    @Session() session: AppSession,
+  ) {
+    return this.service.findById(session.user.id, id);
+  }
+}
+
+@UseGuards(AccessGuard)
+@Controller('users/:userId/addresses')
+export class UserAddressesController {
+  constructor(private readonly service: AddressService) {}
+
+  @Get(':id')
+  @RequirePermission(AllPermissions.address.AddressReadLessOrEqual)
+  async findById(
+    @Param('id', new ParseUUIDPipe({ version: '7' })) id: string,
+    @Param('userId', new ParseUUIDPipe({ version: '7' })) userId: string,
+  ) {
+    return this.service.findById(userId, id);
+  }
+  @Get()
+  @RequirePermission(AllPermissions.address.AddressReadLessOrEqual)
+  async findAllForUser(
+    @Param('userId', new ParseUUIDPipe({ version: '7' })) userId: string,
+  ) {
+    return await this.service.findAll(userId);
+  }
+}
