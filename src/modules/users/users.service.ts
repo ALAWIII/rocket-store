43b8b62a11bf6b6cdb917fd2f7443db9667da6ba
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-27 13:28:46 +0300

    refactor: remove findAll and its input dto.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { IUserRepository } from './infrastructure/repositories/user.repository';
 import { FindUsersByParamsDto } from './dto/find-users-by-filter.dto';
-import { FindAllUsersDto } from './dto/find-all-users.dto';
 import { AssignRoleToUsersDto } from './dto/assign-role-to-users.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserResponseDto } from './dto/user-response.dto';
@@ -10,15 +9,7 @@ import { FindUsersResponseDto } from './dto/find-users-response.dto';
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepo: IUserRepository) {}
-  async findAll(
-    requesterRoleId: string,
-    d: FindAllUsersDto,
-  ): Promise<FindUsersResponseDto> {
-    const result = (
-      await this.userRepo.findAll({ ...d, requesterRoleId })
-    ).unwrap();
-    return { users: result.users.map((u) => u.toJSON()), total: result.total };
-  }
+
   async findMe(id: string): Promise<UserResponseDto> {
     const user = (await this.userRepo.findMe(id)).unwrap();
     return user.toJSON();
