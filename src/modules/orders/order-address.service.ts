3020a91f86accb3a5855e401742a44dd5ee1d58d
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-28 19:16:01 +0300

    refactor(service): annotate the return type and update createOAdrs input parameters to include orderId.

--- src/modules/orders/order-address.service.ts
+++ src/modules/orders/order-address.service.ts
@@ -1,18 +1,28 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { IOrderAddressRepository } from './infrastructure/repositories/order-address.repository';
 import { CreateOrderAddressDto } from './dto/create-order-address.dto';
+import { OrderAddressResponseDto } from './dto/order-address-response.dto';
 
 @Injectable()
 export class OrderAddressService {
   constructor(private readonly oAdrsRepo: IOrderAddressRepository) {}
 
-  async findByOrderId(userId: string, orderId: string) {
+  async findByOrderId(
+    userId: string,
+    orderId: string,
+  ): Promise<OrderAddressResponseDto[]> {
     const oadrs = (
       await this.oAdrsRepo.findByOrderId(userId, orderId)
     ).unwrap();
     return oadrs.map((oad) => oad.toJSON());
   }
-  async createOAdrs(userId: string, data: CreateOrderAddressDto) {
-    return (await this.oAdrsRepo.create(userId, data)).unwrap().toJSON();
+  async createOAdrs(
+    userId: string,
+    orderId: string,
+    data: CreateOrderAddressDto,
+  ): Promise<OrderAddressResponseDto> {
+    return (await this.oAdrsRepo.create(userId, { orderId, ...data }))
+      .unwrap()
+      .toJSON();
   }
 }
