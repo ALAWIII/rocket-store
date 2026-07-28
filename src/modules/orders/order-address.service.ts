ba526e1ec79d21a439e495673cad6d6285d610c7
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-28 18:43:01 +0300

    refactor: remove findById and update findByOrderId to pass userId.

--- src/modules/orders/order-address.service.ts
+++ src/modules/orders/order-address.service.ts
@@ -5,18 +5,11 @@ import { CreateOrderAddressDto } from './dto/create-order-address.dto';
 @Injectable()
 export class OrderAddressService {
   constructor(private readonly oAdrsRepo: IOrderAddressRepository) {}
-  async findById(adrsId: string) {
-    const oadrs = (await this.oAdrsRepo.findById(adrsId)).unwrap();
-    if (oadrs.isNone()) {
-      throw new NotFoundException(
-        `Order Address with id: ${adrsId} is not found.`,
-      );
-    }
-    return oadrs.unwrap().toJSON();
-  }
-  async findByOrderId(orderId: string) {
-    const oadrs = (await this.oAdrsRepo.findByOrderId(orderId)).unwrap();
 
+  async findByOrderId(userId: string, orderId: string) {
+    const oadrs = (
+      await this.oAdrsRepo.findByOrderId(userId, orderId)
+    ).unwrap();
     return oadrs.map((oad) => oad.toJSON());
   }
   async createOAdrs(userId: string, data: CreateOrderAddressDto) {
