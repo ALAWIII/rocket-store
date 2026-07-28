dbb83e37cd5bfe411d86d2de7311612400b0c7ff
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-28 19:14:28 +0300

    refactor(dto): remove orderId field.

--- src/modules/orders/dto/create-order-address.dto.ts
+++ src/modules/orders/dto/create-order-address.dto.ts
@@ -3,8 +3,6 @@ import { IsIn, IsUUID } from 'class-validator';
 export class CreateOrderAddressDto {
   @IsUUID('7')
   addressId!: string;
-  @IsUUID('7')
-  orderId!: string;
   @IsIn(['billing', 'shipping'])
   addressType!: 'billing' | 'shipping';
 }
