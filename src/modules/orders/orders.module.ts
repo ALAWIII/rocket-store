af7b524474e3e8316907feb43a543a9cd0eeb11d
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-07-30 14:49:09 +0300

    refactor: attach entites to orders modules;

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderAddressEntity } from './infrastructure/entities/order-address.entity';
+import { OrderEntity } from './infrastructure/entities/order.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([OrderEntity, OrderAddressEntity])],
+})
 export class OrdersModule {}
