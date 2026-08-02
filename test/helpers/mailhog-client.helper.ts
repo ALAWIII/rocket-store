47b1f445a62599d1e5435229f130a89f7602e5ac
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-02 12:58:29 +0300

    fix: change port from smtp 1025 to http 8025.

--- test/helpers/mailhog-client.helper.ts
+++ test/helpers/mailhog-client.helper.ts
@@ -6,6 +6,6 @@ export function createMailhogClient(options?: {
 }): MailhogClient {
   return new MailhogClient({
     host: options?.host ?? '127.0.0.1',
-    port: options?.port ?? 1025,
+    port: options?.port ?? 8025,
   });
 }
