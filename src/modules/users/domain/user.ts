4f1d39e314c7511ac3cd813f5335b7ef5fbe2590
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-07 20:32:53 +0300

    refactor(user): make givenName, familyName undefined and nullable.
    - remove update method and all its related setters methods.
    - remove UpdateUserProps type.
    - add optional closure for providing nullable values.

--- src/modules/users/domain/user.ts
+++ src/modules/users/domain/user.ts
@@ -10,8 +10,8 @@ type UserProps = {
   readonly id: UserId;
   email: Email;
   name: Name;
-  givenName: Name;
-  familyName: Name;
+  givenName?: Name | null;
+  familyName?: Name | null;
   roleId: string;
   image?: string;
   phone?: Phone;
@@ -22,8 +22,8 @@ type UserPrimitives = {
   id: string;
   email: string;
   name: string;
-  givenName: string;
-  familyName: string;
+  givenName?: string | null;
+  familyName?: string | null;
   roleId: string;
   image?: string;
   phone?: string;
@@ -31,29 +31,29 @@ type UserPrimitives = {
   createdAt: Date;
 };
 
-type UpdateUserProps = Partial<
-  Pick<UserProps, 'name' | 'givenName' | 'familyName' | 'roleId' | 'phone'>
->;
 export class User {
   private constructor(private data: UserProps) {}
   static restore(props: UserProps) {
     return new User(props);
   }
   static fromPrimitives(data: UserPrimitives): Result<User, ValueObjectError> {
-    const dataValidate = unwrapResultObject({
+    const optional = <T, R>(
+      value: T | null | undefined,
+      create: (value: T) => R,
+    ) => (value == null ? Ok(undefined) : create(value));
+    const dataValidated = unwrapResultObject({
       email: Email.create(data.email),
       name: Name.create(data.name),
-      givenName: Name.create(data.givenName),
-      familyName: Name.create(data.familyName),
-      phone:
-        data.phone !== undefined ? Phone.create(data.phone) : Ok(undefined),
+      givenName: optional(data.givenName, (value) => Name.create(value)),
+      familyName: optional(data.familyName, (value) => Name.create(value)),
+      phone: optional(data.phone, (value) => Phone.create(value)),
     });
-    if (dataValidate.isErr()) return Err(dataValidate.error);
+    if (dataValidated.isErr()) return Err(dataValidated.error);
 
     return Ok(
       new User({
         id: UserId.create(data.id),
-        ...dataValidate.value,
+        ...dataValidated.value,
         image: data.image,
         roleId: data.roleId,
         createdAt: data.createdAt,
@@ -61,39 +61,14 @@ export class User {
       }),
     );
   }
-  update(props: UpdateUserProps) {
-    if (props.givenName !== undefined) this.changeFirstName(props.givenName);
-    if (props.familyName !== undefined) this.changeLastName(props.familyName);
-    if (props.roleId !== undefined) this.changeRole(props.roleId);
-    if (props.phone !== undefined) this.changePhone(props.phone);
-    this.touch();
-  }
 
-  private changeFirstName(fname: Name) {
-    this.data.givenName = fname;
-  }
-  private changeLastName(lastName: Name) {
-    this.data.familyName = lastName;
-  }
-
-  private changePhone(phone: Phone) {
-    this.data.phone = phone;
-  }
-
-  private changeRole(roleId: string) {
-    this.data.roleId = roleId;
-  }
-
-  private touch() {
-    this.data.updatedAt = new Date();
-  }
   toJSON(): UserPrimitives {
     return {
       id: this.data.id.toString(),
       name: this.data.name.value,
       email: this.data.email.value,
-      givenName: this.data.givenName.value,
-      familyName: this.data.familyName.value,
+      givenName: this.data.givenName?.value,
+      familyName: this.data.familyName?.value,
       roleId: this.data.roleId,
       image: this.data.image,
       phone: this.data.phone?.value,
