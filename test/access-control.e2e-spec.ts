354e3ca9a8b12c8f6ca51c9f7a16eeb8baa2534b
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-08 17:19:06 +0300

    test: add get roles 403 forbidden test.

--- test/access-control.e2e-spec.ts
+++ test/access-control.e2e-spec.ts
@@ -1,7 +1,10 @@
 import { MailhogClient } from 'mailhog-awesome';
 import { TestApp } from './helpers/app-test.helper';
 import { TestDatabase } from './helpers/database-test.helper';
-import { AuthUserResult } from './helpers/auth-user-flow.builder';
+import {
+  AuthUserResult,
+  UserAuthFlowBuilder,
+} from './helpers/auth-user-flow.builder';
 import { createAuthenticatedTestContext } from './fixtures/create-authenticated-test-context.fixture';
 import { SYSTEM_ROLES } from 'src/modules/access-control/application/system-roles/system-roles.definition';
 
@@ -20,6 +23,20 @@ describe('access-control (e2e)', () => {
         .get('/api/v1/roles')
         .expect(200);
       expect(response.body).toEqual(SYSTEM_ROLES.map((r) => r.toJSON()));
+    });
+    it('should return 403 forbidden when an unauthorized user request roles without having roles.read permission.', async () => {
+      const userAgent = app.createAgent();
+      const customer = await UserAuthFlowBuilder.create({
+        dbDataSource: db.dataSource,
+        mailhogClient: mailClient,
+        userAgent,
+      })
+        .random()
+        .asRole('customer')
+        .verified()
+        .signin()
+        .build();
+      await customer.userAgent.get('/api/v1/roles').expect(403);
     });
   });
   afterEach(async () => {
