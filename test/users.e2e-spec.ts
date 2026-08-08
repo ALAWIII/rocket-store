0935d7ec87a124c3b2ee9e310ff92db941e65893
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-08 13:45:57 +0300

    feat: finish implementing findMe endpoint.

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,23 +1,31 @@
 import { MailhogClient } from 'mailhog-awesome';
 import { TestApp } from './helpers/app-test.helper';
 import { TestDatabase } from './helpers/database-test.helper';
-import { BuildResult } from './helpers/signup-user-flow.builder';
+import { SignupResult } from './helpers/signup-user-flow.builder';
 import { createAuthenticatedTestContext } from './fixtures/create-authenticated-test-context.fixture';
 
 describe('users (e2e)', () => {
   let app: TestApp;
   let db: TestDatabase;
   let mailClient: MailhogClient;
-  let adminUser: BuildResult;
+  let adminUser: SignupResult;
   beforeEach(async () => {
     ({ app, db, mailClient, adminUser } =
       await createAuthenticatedTestContext());
   });
-  describe('GET /api/v1/users/me', () => {
+  describe('GET /api/v1/users/me (findMe)', () => {
     it('should return user profile.', async () => {
-      const response = await adminUser.userAgent.get('/api/v1/users/me');
-
-      console.log(response.status, response.body);
+      const userResp = await adminUser.userAgent
+        .get('/api/v1/users/me')
+        .expect(200);
+      expect(userResp.body).toEqual({
+        id: adminUser.userDb.id,
+        name: adminUser.userDb.name,
+        email: adminUser.userDb.email,
+        roleId: adminUser.userDb.roleId,
+        createdAt: adminUser.userDb.createdAt,
+        updatedAt: adminUser.userDb.updatedAt,
+      });
     });
   });
   describe('GET /api/auth/get-session', () => {
