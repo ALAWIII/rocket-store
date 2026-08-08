108b592c03efc16f538d4ac43b8b11db0b1192e9
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-08 16:17:04 +0300

    test: update GET roles test and add expect to it.

--- test/access-control.e2e-spec.ts
+++ test/access-control.e2e-spec.ts
@@ -3,6 +3,7 @@ import { TestApp } from './helpers/app-test.helper';
 import { TestDatabase } from './helpers/database-test.helper';
 import { SignupResult } from './helpers/signup-user-flow.builder';
 import { createAuthenticatedTestContext } from './fixtures/create-authenticated-test-context.fixture';
+import { SYSTEM_ROLES } from 'src/modules/access-control/application/system-roles/system-roles.definition';
 
 describe('access-control (e2e)', () => {
   let app: TestApp;
@@ -15,9 +16,10 @@ describe('access-control (e2e)', () => {
   });
   describe('GET /api/v1/roles', () => {
     it('should return all roles for admin user.', async () => {
-      const response = await adminUser.userAgent.get('/api/v1/roles');
-
-      console.log(response.status, response.body);
+      const response = await adminUser.userAgent
+        .get('/api/v1/roles')
+        .expect(200);
+      expect(response.body).toEqual(SYSTEM_ROLES.map((r) => r.toJSON()));
     });
   });
   afterEach(async () => {
