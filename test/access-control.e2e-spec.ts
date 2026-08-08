60e57f7d8595197eaa9720f1202b17ffcb5a12bb
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-08 16:59:42 +0300

    refactor: update import names.

--- test/access-control.e2e-spec.ts
+++ test/access-control.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { MailhogClient } from 'mailhog-awesome';
 import { TestApp } from './helpers/app-test.helper';
 import { TestDatabase } from './helpers/database-test.helper';
-import { SignupResult } from './helpers/signup-user-flow.builder';
+import { AuthUserResult } from './helpers/auth-user-flow.builder';
 import { createAuthenticatedTestContext } from './fixtures/create-authenticated-test-context.fixture';
 import { SYSTEM_ROLES } from 'src/modules/access-control/application/system-roles/system-roles.definition';
 
@@ -9,7 +9,7 @@ describe('access-control (e2e)', () => {
   let app: TestApp;
   let db: TestDatabase;
   let mailClient: MailhogClient;
-  let adminUser: SignupResult;
+  let adminUser: AuthUserResult;
   beforeEach(async () => {
     ({ app, db, mailClient, adminUser } =
       await createAuthenticatedTestContext());
