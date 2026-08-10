55379d612003c2be991046060b7b3e0c97e23033
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-10 11:07:03 +0300

    refactor: register RoleError to be its own prototype.

--- src/modules/access-control/domain/role.error.ts
+++ src/modules/access-control/domain/role.error.ts
@@ -1,5 +1,11 @@
-export abstract class RoleError extends Error {}
+export abstract class RoleError extends Error {
+  constructor(message: string, cause?: unknown) {
+    super(message, { cause });
 
+    this.name = new.target.name;
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
 export class InvalidRoleValueError extends RoleError {}
 
 export class InvalidPermissionSupersetError extends RoleError {}
