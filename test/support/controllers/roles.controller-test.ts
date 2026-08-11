d540dc2b8aeeb4ae38440f05980699567343721f
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-11 19:04:47 +0300

    test(controllers): update methods to pass statusCodes parameter direcrly to statusCodesListNormalize.

--- test/support/controllers/roles.controller-test.ts
+++ test/support/controllers/roles.controller-test.ts
@@ -5,7 +5,7 @@ import {
   parseResponseBody,
   statusCodesListNormalize,
 } from '../utils/parse-response-body.util';
-import { TestStatusCodes } from '../types/status-codes.type';
+import { ExpectedTestStatusCode } from '../types/expected-test-status-code.type';
 
 type CreateRoleTestPayload = Omit<RoleTestDto, 'id'>;
 type CreateRoleTestBody = { response: Response; body?: RoleTestDto };
@@ -15,69 +15,69 @@ export class RolesControllerTest {
   withAgent(agent: UserAgent): RolesControllerTest {
     return new RolesControllerTest(agent);
   }
-  async reloadPolicies(statusCodes: TestStatusCodes): Promise<{
+  async reloadPolicies(statusCodes: ExpectedTestStatusCode): Promise<{
     response: Response;
     body?: { attempt: number };
   }> {
     const response = await this.agent
       .post(`/api/v1/roles/policies/reload`)
-      .expect(statusCodes.expectedStatusCode);
+      .expect(statusCodes.code);
     const body = parseResponseBody<{ attempt: number }>(
       response,
-      statusCodesListNormalize(statusCodes.bodyParseStatusCode),
+      statusCodesListNormalize(statusCodes),
     );
     return { response, body };
   }
   async create(
     role: CreateRoleTestPayload,
-    statusCodes: TestStatusCodes,
+    statusCodes: ExpectedTestStatusCode,
   ): Promise<CreateRoleTestBody> {
     const response = await this.agent
       .post(`/api/v1/roles`)
       .send(role)
-      .expect(statusCodes.expectedStatusCode);
+      .expect(statusCodes.code);
     const body = parseResponseBody<RoleTestDto>(
       response,
-      statusCodesListNormalize(statusCodes.bodyParseStatusCode),
+      statusCodesListNormalize(statusCodes),
     );
     return { response, body };
   }
   async findAll(
-    statusCodes: TestStatusCodes,
+    statusCodes: ExpectedTestStatusCode,
     scope?: 'assignable' | 'creatable',
   ): Promise<{ response: Response; body?: RoleTestDto[] }> {
     const query = scope ? `?scope=${scope}` : '';
     const response = await this.agent
       .get(`/api/v1/roles${query}`)
-      .expect(statusCodes.expectedStatusCode);
+      .expect(statusCodes.code);
     const body = parseResponseBody<RoleTestDto[]>(
       response,
-      statusCodesListNormalize(statusCodes.bodyParseStatusCode),
+      statusCodesListNormalize(statusCodes),
     );
     return { response, body };
   }
   async update(
     roleId: string,
     name: string,
-    statusCodes: TestStatusCodes,
+    statusCodes: ExpectedTestStatusCode,
   ): Promise<CreateRoleTestBody> {
     const response: Response = await this.agent
       .put(`/api/v1/roles/${roleId}`)
       .send({ name })
-      .expect(statusCodes.expectedStatusCode);
+      .expect(statusCodes.code);
     const body = parseResponseBody<RoleTestDto>(
       response,
-      statusCodesListNormalize(statusCodes.bodyParseStatusCode),
+      statusCodesListNormalize(statusCodes),
     );
     return { response, body };
   }
-  async remove(roleId: string, statusCodes: TestStatusCodes) {
+  async remove(roleId: string, statusCodes: ExpectedTestStatusCode) {
     const response = await this.agent
       .delete(`/api/v1/roles/${roleId}`)
-      .expect(statusCodes.expectedStatusCode);
+      .expect(statusCodes.code);
     const body = parseResponseBody<{ affected: number }>(
       response,
-      statusCodesListNormalize(statusCodes.bodyParseStatusCode),
+      statusCodesListNormalize(statusCodes),
     );
     return { response, body };
   }
