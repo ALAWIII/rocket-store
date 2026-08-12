6c2e51f923ba6440f6d870da41045dcb91c46af8
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-12 07:39:45 +0300

    test(support): add statusCode parameter.

--- test/support/controllers/users/users.controller-test.ts
+++ test/support/controllers/users/users.controller-test.ts
@@ -1,11 +1,12 @@
 import { UserAgent } from 'test/support/helpers/app-test.helper';
+import { ExpectedTestStatusCode } from 'test/support/types/expected-test-status-code.type';
 
 export class UsersControllerTest {
   constructor(private readonly agent: UserAgent) {}
   withAgent(agent: UserAgent): UsersControllerTest {
     return new UsersControllerTest(agent);
   }
-  findMe() {}
+  findMe(statusCode: ExpectedTestStatusCode) {}
   findAll() {}
   findById() {}
   updateMe() {}
