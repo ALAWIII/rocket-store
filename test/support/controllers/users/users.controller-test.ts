1458a2b2833f225029c95af39b9e0ffed17fb37f
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-13 12:21:13 +0300

    test(support): implement findMe, findAll, findById for UsersControllerTest

--- test/support/controllers/users/users.controller-test.ts
+++ test/support/controllers/users/users.controller-test.ts
@@ -1,15 +1,47 @@
 import { UserAgent } from 'test/support/helpers/app-test.helper';
 import { ExpectedTestStatusCode } from 'test/support/types/expected-test-status-code.type';
+import { UserTestDto } from 'test/support/types/user.dto.type';
+import {
+  parseResponseBody,
+  statusCodesListNormalize,
+} from 'test/support/utils/parse-response-body.util';
 
 export class UsersControllerTest {
   constructor(private readonly agent: UserAgent) {}
   withAgent(agent: UserAgent): UsersControllerTest {
     return new UsersControllerTest(agent);
   }
-  findMe(statusCode: ExpectedTestStatusCode) {}
-  findAll() {}
-  findById() {}
-  updateMe() {}
-  assignRole() {}
-  reassignUsersRole() {}
+  async findMe(statusCode: ExpectedTestStatusCode) {
+    const response = await this.agent
+      .get('/api/v1/users/me')
+      .expect(statusCode.code);
+    const body = parseResponseBody<UserTestDto>(
+      response,
+      statusCodesListNormalize(statusCode),
+    );
+    return { response, body };
+  }
+  async findAll(statusCode: ExpectedTestStatusCode) {
+    const response = await this.agent
+      .get('/api/v1/users')
+      .expect(statusCode.code);
+    const body = parseResponseBody<{ users: UserTestDto[]; total: number }>(
+      response,
+      statusCodesListNormalize(statusCode),
+    );
+    return { response, body };
+  }
+  async findById(userId: string, statusCode: ExpectedTestStatusCode) {
+    const response = await this.agent
+      .get(`/api/v1/users/${userId}`)
+      .expect(statusCode.code);
+    const body = parseResponseBody<UserTestDto>(
+      response,
+      statusCodesListNormalize(statusCode),
+    );
+    return { response, body };
+  }
+  updateMe(statusCode: ExpectedTestStatusCode) {}
+  assignRole(statusCode: ExpectedTestStatusCode) {}
+  reassignUsersRole(statusCode: ExpectedTestStatusCode) {}
 }
