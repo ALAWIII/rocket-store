001174c9c073115bd580db356cbfe492cd5c2e99
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-14 20:04:56 +0300

    test(utils): add pickFrom that helps in extracting common fields of a given objects.

---
+++ test/support/utils/pick-from.util.ts
@@ -0,0 +1,23 @@
+// extract common keys of shape and given object.
+type CommonKey<T extends object, U extends object> = Extract<keyof T, keyof U>;
+
+type PickFrom<T extends object, U extends object> = {
+  [K in CommonKey<T, U>]: U[K];
+};
+
+export function pickFrom<T extends object, U extends object>(
+  shape: T,
+  object: U,
+): PickFrom<T, U> {
+  const result = {} as PickFrom<T, U>;
+
+  for (const key of Object.keys(shape)) {
+    if (key in object) {
+      const commonKey = key as CommonKey<T, U>;
+
+      (result as Record<string, unknown>)[key] = object[commonKey as keyof U];
+    }
+  }
+
+  return result;
+}
