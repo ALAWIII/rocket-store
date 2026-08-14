e28d16872cfecc0f6ff67af2386ec74345f1bd1c
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-14 20:02:51 +0300

    refactor: annotate the return types of the controller methods.

--- src/modules/users/addresses.controller.ts
+++ src/modules/users/addresses.controller.ts
@@ -15,24 +15,28 @@ import { RequirePermission } from '../shared/authorization/decorators/require-pe
 import { AllPermissions } from '../access-control/domain/permission';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
+import { AddressResponseDto } from './dto/address-response.dto';
 
 @Controller('users/me/addresses')
 export class MyAddressesController {
   constructor(private readonly service: AddressService) {}
 
   @Get()
-  async findAll(@Session() session: AppSession) {
+  async findAll(@Session() session: AppSession): Promise<AddressResponseDto[]> {
     return await this.service.findAll(session.user.id);
   }
   @Get(':id')
   async findById(
     @Param('id', new ParseUUIDPipe({ version: '7' })) id: string,
     @Session() session: AppSession,
-  ) {
+  ): Promise<AddressResponseDto> {
     return this.service.findById(session.user.id, id);
   }
   @Post()
-  async create(@Session() session: AppSession, @Body() d: CreateAddressDto) {
+  async create(
+    @Session() session: AppSession,
+    @Body() d: CreateAddressDto,
+  ): Promise<AddressResponseDto> {
     return await this.service.createAdrs(session.user.id, d);
   }
   @Put(':id')
@@ -40,15 +44,15 @@ export class MyAddressesController {
     @Session() session: AppSession,
     @Param('id', new ParseUUIDPipe({ version: '7' })) id: string,
     @Body() d: UpdateAddressDto,
-  ) {
+  ): Promise<AddressResponseDto> {
     return await this.service.updateAdrs(session.user.id, id, d);
   }
   @Delete(':id')
   async delete(
     @Session() session: AppSession,
     @Param('id', new ParseUUIDPipe({ version: '7' })) id: string,
-  ) {
-    return this.service.deleteAdrs(session.user.id, id);
+  ): Promise<{ affected: number }> {
+    return { affected: await this.service.deleteAdrs(session.user.id, id) };
   }
 }
 
@@ -61,14 +65,14 @@ export class UserAddressesController {
   async findById(
     @Param('id', new ParseUUIDPipe({ version: '7' })) id: string,
     @Param('userId', new ParseUUIDPipe({ version: '7' })) userId: string,
-  ) {
+  ): Promise<AddressResponseDto> {
     return this.service.findById(userId, id);
   }
   @Get()
   @RequirePermission(AllPermissions.address.AddressReadLessOrEqual)
   async findAllForUser(
     @Param('userId', new ParseUUIDPipe({ version: '7' })) userId: string,
-  ) {
+  ): Promise<AddressResponseDto[]> {
     return await this.service.findAll(userId);
   }
 }
