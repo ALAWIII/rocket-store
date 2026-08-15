9633dacc097dc5435192e7878d150e68e38c9645
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-15 09:49:09 +0300

    test(address): add success return all addresse for the requester user.

--- test/users/addresses.e2e-spec.ts
+++ test/users/addresses.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { it } from 'test/support/fixtures/authenticated-e2e.fixture';
+import { AddressTestDto } from 'test/support/types/user/address.dto.type';
 import { createRandomAddress } from 'test/support/utils/create-random-address.util';
 import { pickFrom } from 'test/support/utils/pick-from.util';
 
@@ -14,6 +15,42 @@ describe.concurrent('addresses (e2e)', () => {
         parseBody: true,
       });
       expect(pickFrom(adrs, response.body!)).toEqual(adrs);
+    });
+  });
+  describe(`GET ${apiPrefix} (findAll)`, () => {
+    it('should successfully return all addresses for the requester user.', async ({
+      myAddressController,
+    }) => {
+      const addresses: AddressTestDto[] = [];
+
+      for (let i = 1; i <= 5; i++) {
+        const response = await myAddressController.create(
+          createRandomAddress(),
+          {
+            code: 201,
+            parseBody: true,
+          },
+        );
+
+        addresses.push(response.body!);
+      }
+
+      const fetchedAddresses = (
+        await myAddressController.findAll({
+          code: 200,
+          parseBody: true,
+        })
+      ).body!;
+
+      const sortById = (a: AddressTestDto, b: AddressTestDto) =>
+        a.id.localeCompare(b.id);
+
+      expect(fetchedAddresses).toHaveLength(addresses.length);
+      expect(fetchedAddresses).toHaveLength(5);
+
+      expect([...fetchedAddresses].sort(sortById)).toEqual(
+        [...addresses].sort(sortById),
+      );
     });
   });
 });
