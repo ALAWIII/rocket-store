cfc0c2f01ee4de89a0e0655189c3c47654b424bf
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-15 19:55:28 +0300

    refactor(images): register ImageEntity to the ImagesModule.

--- src/modules/images/images.module.ts
+++ src/modules/images/images.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ImageEntity } from './infrastructure/entities/image.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({ imports: [TypeOrmModule.forFeature([ImageEntity])] })
 export class ImagesModule {}
