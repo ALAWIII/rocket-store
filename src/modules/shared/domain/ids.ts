fe131ad8dd218468fd01ae182375b1133405d7d0
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-16 13:09:44 +0300

    refactor(shared): replace all sub-classes with Id parent class.
    - replace ids subclasses implementations with shallow once extending Id class.
    - update create static method logic to call the subclass constructor.
    - attach IdError on validate with the name of subclass as part of error message.

--- src/modules/shared/domain/ids.ts
+++ src/modules/shared/domain/ids.ts
@@ -1,11 +1,12 @@
 import { v7, validate, version } from 'uuid';
+import { IdError } from './id.error';
 
 export abstract class UuidV7Id {
   protected constructor(private readonly value: string) {}
 
   protected static validate(value: string) {
     if (!validate(value) || version(value) !== 7) {
-      throw new Error('Id must be UUID v7');
+      throw new IdError(`${this.name} must be UUID v7`);
     }
   }
 
@@ -20,328 +21,43 @@ export abstract class UuidV7Id {
     return this.value;
   }
 }
-
-export class UserId extends UuidV7Id {
-  private constructor(value: string) {
+export class Id extends UuidV7Id {
+  constructor(value: string) {
     super(value);
   }
 
-  static create(value?: string): UserId {
+  static create<T extends typeof Id>(this: T, value?: string): InstanceType<T> {
     const id = value ?? v7();
-    super.validate(id);
-    return new UserId(id);
+    this.validate(id); // 'this' is the subclass (OrderId, UserId, ...)
+    return new this(id) as InstanceType<T>;
   }
 }
-
-export class OrderId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): OrderId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new OrderId(id);
-  }
-}
-export class OrderItemId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): OrderItemId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new OrderItemId(id);
-  }
-}
-
-export class BrandId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): BrandId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new BrandId(id);
-  }
-}
-export class ProductId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): ProductId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new ProductId(id);
-  }
-}
-export class ProductVariantId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): ProductVariantId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new ProductVariantId(id);
-  }
-}
-export class PageTemplateId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): PageTemplateId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new PageTemplateId(id);
-  }
-}
-export class CategoryId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): CategoryId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new CategoryId(id);
-  }
-}
-export class CartItemId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): CartItemId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new CartItemId(id);
-  }
-}
-export class AddressId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): AddressId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new AddressId(id);
-  }
-}
-
-export class PaymentId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): PaymentId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new PaymentId(id);
-  }
-}
-
-export class PaymentTransactionId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): PaymentTransactionId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new PaymentTransactionId(id);
-  }
-}
-
-export class PaymentProviderId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): PaymentProviderId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new PaymentProviderId(id);
-  }
-}
-export class ShippingProviderId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): ShippingProviderId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new ShippingProviderId(id);
-  }
-}
-export class ShipmentId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): ShipmentId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new ShipmentId(id);
-  }
-}
-export class WishlistId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): WishlistId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new WishlistId(id);
-  }
-}
-export class WishlistItemId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): WishlistItemId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new WishlistItemId(id);
-  }
-}
-export class ImageId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): ImageId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new ImageId(id);
-  }
-}
-export class ImageAttachmentId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): ImageAttachmentId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new ImageAttachmentId(id);
-  }
-}
-export class PromotionId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): PromotionId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new PromotionId(id);
-  }
-}
-export class PromotionTargetId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): PromotionTargetId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new PromotionTargetId(id);
-  }
-}
-export class PromotionRuleId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): PromotionRuleId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new PromotionRuleId(id);
-  }
-}
-export class CouponId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): CouponId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new CouponId(id);
-  }
-}
-export class PromotionRedemptionId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): PromotionRedemptionId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new PromotionRedemptionId(id);
-  }
-}
-export class ReviewId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): ReviewId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new ReviewId(id);
-  }
-}
-export class AuditLogId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): AuditLogId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new AuditLogId(id);
-  }
-}
-export class CartId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): CartId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new CartId(id);
-  }
-}
-export class RoleId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): RoleId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new RoleId(id);
-  }
-}
-export class BrandImageId extends UuidV7Id {
-  private constructor(value: string) {
-    super(value);
-  }
-
-  static create(value?: string): BrandImageId {
-    const id = value ?? v7();
-    super.validate(id);
-    return new BrandImageId(id);
-  }
-}
+export class UserId extends Id {}
+export class RoleId extends Id {}
+export class ProductId extends Id {}
+export class ProductVariantId extends Id {}
+export class OrderId extends Id {}
+export class OrderItemId extends Id {}
+export class ImageId extends Id {}
+export class ImageAttachmentId extends Id {}
+export class BrandId extends Id {}
+export class BrandImageId extends Id {}
+export class PageTemplateId extends Id {}
+export class CategoryId extends Id {}
+export class CartItemId extends Id {}
+export class AddressId extends Id {}
+export class PaymentId extends Id {}
+export class PaymentTransactionId extends Id {}
+export class PaymentProviderId extends Id {}
+export class ShippingProviderId extends Id {}
+export class ShipmentId extends Id {}
+export class WishlistId extends Id {}
+export class WishlistItemId extends Id {}
+export class PromotionId extends Id {}
+export class PromotionTargetId extends Id {}
+export class PromotionRuleId extends Id {}
+export class CouponId extends Id {}
+export class PromotionRedemptionId extends Id {}
+export class ReviewId extends Id {}
+export class AuditLogId extends Id {}
+export class CartId extends Id {}
