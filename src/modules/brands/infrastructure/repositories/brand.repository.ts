7c5cb1a32f57a462b0e6bda05c5298f1b7e7faaa
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-16 16:06:42 +0300

    feat(brands): add IBrandRepository base class.

--- src/modules/brands/infrastructure/repositories/brand.repository.ts
+++ src/modules/brands/infrastructure/repositories/brand.repository.ts
@@ -1,1 +1,23 @@
-export abstract class IBrandRepository {}
+import { DBResult } from 'src/modules/shared/errors/error.types';
+import { Brand } from '../../domain/brand';
+type FindOptions = {
+  includeLogo?: boolean;
+  includeBanners?: boolean;
+  page?: number;
+  limit?: number;
+};
+export abstract class IBrandRepository {
+  abstract findAll(options?: FindOptions): Promise<DBResult<Brand[]>>;
+  abstract findById(
+    id: string,
+    options?: FindOptions,
+  ): Promise<DBResult<Brand>>;
+  abstract findByName(
+    name: string,
+    options?: FindOptions,
+  ): Promise<DBResult<Brand>>;
+
+  abstract create(brand: Brand): Promise<DBResult<Brand>>;
+  abstract update(brand: Brand): Promise<DBResult<Brand>>;
+  abstract delete(id: string): Promise<DBResult<number>>;
+}
