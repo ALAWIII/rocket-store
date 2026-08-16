f87d4965cca7e55fdf822f973f7bdad49d764539
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-16 13:41:03 +0300

    feat(brand): add toJSON,restore methods and BrandImagePrimitives,BrandImageRole types

--- src/modules/brands/domain/brand-image.ts
+++ src/modules/brands/domain/brand-image.ts
@@ -1,17 +1,21 @@
 import { BrandImageId, ImageId } from 'src/modules/shared/domain/ids';
-
+export type BrandImageRole = 'banner' | 'logo';
 type BrandImageProps = {
   id: BrandImageId;
   imageId: ImageId;
-  imageRole: 'banner' | 'logo';
+  imageRole: BrandImageRole;
   sortOrder: number;
   createdAt: Date;
   updatedAt: Date;
 };
 type CreateBrandImageProps = {
   imageId: string;
-  imageRole: 'banner' | 'logo';
+  imageRole: BrandImageRole;
   sortOrder: number;
+};
+type BrandImagePrimitives = Omit<BrandImageProps, 'id' | 'imageId'> & {
+  id: string;
+  imageId: string;
 };
 export class BrandImage {
   private constructor(private props: BrandImageProps) {}
@@ -26,5 +30,19 @@ export class BrandImage {
       createdAt: newDate,
       updatedAt: newDate,
     });
+  }
+  static restore(data: BrandImagePrimitives) {
+    return new BrandImage({
+      ...data,
+      id: BrandImageId.create(data.id),
+      imageId: ImageId.create(data.imageId),
+    });
+  }
+  toJSON(): BrandImagePrimitives {
+    return {
+      ...this.props,
+      id: this.props.id.toString(),
+      imageId: this.props.imageId.toString(),
+    };
   }
 }
