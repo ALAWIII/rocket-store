7076f0593b515e9ee38af502b761decb55cceb1e
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-17 18:41:26 +0300

    refactor: register BrandImagesEntity on BrandsModule.

--- src/modules/brands/brands.module.ts
+++ src/modules/brands/brands.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BrandEntity } from './infrastructure/entities/brand.entity';
+import { BrandImagesEntity } from './infrastructure/entities/brand-images.entity';
 
-@Module({ imports: [TypeOrmModule.forFeature([BrandEntity])] })
+@Module({
+  imports: [TypeOrmModule.forFeature([BrandEntity, BrandImagesEntity])],
+})
 export class BrandsModule {}
