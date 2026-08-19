26471d45d1018bedb839a2e2ff4c86dba78fc7fe
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-19 13:42:09 +0300

    feat: add fields to ImageEntity and update types.
    - add checksum field.
    - add uploadedBy foreign key field points to users table.
    - make dimensions fields not null.
    - make altText 125 character length.
    - remove storageKey, metadata and url fields.

--- src/modules/images/infrastructure/entities/image.entity.ts
+++ src/modules/images/infrastructure/entities/image.entity.ts
@@ -1,29 +1,29 @@
 import { CreateDateColumnTz } from 'src/modules/shared/database/decorators/timestamptz-data-column.decorator';
 import { UuidV7PrimaryColumn } from 'src/modules/shared/database/decorators/uuidv7-primary-column.decorator';
-import { Column, Entity } from 'typeorm';
+import { UserEntity } from 'src/modules/users/infrastructure/entities/user.entity';
+import { Column, Entity, ForeignKey } from 'typeorm';
 
 @Entity('images')
 export class ImageEntity {
   @UuidV7PrimaryColumn()
   id!: string;
-  @Column('uuid', { unique: true })
-  storageKey!: string;
   @Column('varchar', { length: 50 })
   name!: string;
-  @Column('text', { nullable: true })
-  url!: string | null;
+  @Column('varchar', { length: 64 })
+  checksum!: string;
   @Column('varchar', { length: 100 })
   mimeType!: string;
   @Column('bigint')
   sizeBytes!: number;
-  @Column('integer', { nullable: true })
-  width!: number | null;
-  @Column('integer', { nullable: true })
-  height!: number | null;
-  @Column('varchar', { length: 100, nullable: true })
+  @Column('integer')
+  width!: number;
+  @Column('integer')
+  height!: number;
+  @Column('varchar', { length: 125, nullable: true })
   altText!: string | null;
-  @Column('jsonb', { nullable: true })
-  metadata!: Record<string, unknown> | null;
+  @Column({ type: 'uuid', nullable: true })
+  @ForeignKey(() => UserEntity, (u) => u.id, { onDelete: 'SET NULL' })
+  uploadedBy!: string | null;
   @CreateDateColumnTz()
   createdAt!: Date;
 }
