f42e78e2f1ad1531d73d60eb9332b9d194b3ed39
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-19 20:41:33 +0300

    feat: implement save method.

---
+++ src/modules/images/infrastructure/repositories/image.typeorm.repository.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { IImageRepository } from './image.repository';
+import { DBResult } from 'src/modules/shared/errors/error.types';
+import { Image } from '../../domain/image';
+import { mapTypeOrmError } from 'src/modules/shared/errors/mappers/database-error.mapper';
+import { Err } from 'ts-results-es';
+import { Repository } from 'typeorm';
+import { ImageEntity } from '../entities/image.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CorruptedPersistenceDataError } from 'src/modules/shared/errors/database.error';
+
+@Injectable()
+export class ImageRepository implements IImageRepository {
+  constructor(
+    @InjectRepository(ImageEntity)
+    private readonly imageRepo: Repository<ImageEntity>,
+  ) {}
+  async save(image: Image): Promise<DBResult<Image>> {
+    try {
+      const imageJson = image.toJSON();
+      const result = await this.imageRepo.save(
+        this.imageRepo.create(imageJson),
+      );
+      return this.toDomain(result);
+    } catch (e) {
+      return Err(mapTypeOrmError(e));
+    }
+  }
+  private toDomain(img: ImageEntity) {
+    return Image.restore({ ...img }).mapErr(
+      (e) =>
+        new CorruptedPersistenceDataError(
+          `Failed to construct image from ImageEntity: ${e.message}`,
+          e,
+        ),
+    );
+  }
+}
