8c7af775a0b2006e1024a331cdc54330ad885f94
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-29 11:30:19 +0300

    feat: add image table usage registery for registering the disjunction images tables.

---
+++ src/modules/shared/domain/image-usage-table.ts
@@ -0,0 +1,10 @@
+export const IMAGE_USAGE_TABLES = [
+  'brand_images',
+  //  'product_images',
+  // 'product_variant_images',
+  //'category_images',
+  // 'user_images',
+  // 'app_images',
+  // add new tables here only
+] as const;
+export const IMAGE_FK_COLUMN = 'imageId';
