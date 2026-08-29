ce4f8740582d1f26630e376f41f71bdd23236df9
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-29 12:30:17 +0300

    feat: add sortBy field to Pagination, add FindUnUsedDbResponse new type.

--- src/modules/images/infrastructure/repositories/image.repository.ts
+++ src/modules/images/infrastructure/repositories/image.repository.ts
@@ -4,12 +4,22 @@ import { Image } from '../../domain/image';
 export type Pagination = {
   page?: number;
   limit?: number;
+  sortBy?: 'name' | 'sizeBytes' | 'createdAt';
 };
-
+export type FindUnUsedDbResponse = {
+  images: Image[];
+  pagination: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+};
 export abstract class IImageRepository {
   abstract save(image: Image): Promise<DBResult<Image>>;
   abstract findById(imageId: string): Promise<DBResult<Image>>;
-  abstract findUnUsed(options: Pagination): Promise<DBResult<Image[]>>;
+  abstract findUnUsed(
+    options: Pagination,
+  ): Promise<DBResult<FindUnUsedDbResponse>>;
   abstract delete(imageId: string): Promise<DBResult<number>>;
   abstract deleteUnUsed(): Promise<DBResult<number>>;
 }
