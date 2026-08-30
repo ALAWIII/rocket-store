d70a5be5113fe8c836c248eddab9a4d0710b162b
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-30 14:15:03 +0300

    feat: add JobsPgBossService implementation for IJobService.

---
+++ src/jobs/jobs.pg-boss.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import {
+  CreateWorkerOptions,
+  IJobsService,
+  JobData,
+  JobId,
+  WorkerId,
+} from './jobs.service';
+import { PgBossCoreService } from './pg-boss.core.service';
+
+@Injectable()
+export class JobsPgBossService implements IJobsService {
+  constructor(private readonly core: PgBossCoreService) {}
+
+  private get boss() {
+    return this.core.getBoss();
+  }
+
+  async sendJob<T extends JobData>(
+    jobKind: string,
+    data: T,
+  ): Promise<JobId | null> {
+    return this.boss.send(jobKind, data);
+  }
+
+  async createWorker<T extends JobData>(
+    jobKind: string,
+    handler: (data: T[]) => Promise<void>,
+    options?: CreateWorkerOptions,
+  ): Promise<WorkerId> {
+    return this.boss.work<T>(
+      jobKind,
+      {
+        batchSize: options?.batchSize ?? 10,
+        pollingIntervalSeconds: 5,
+        notifyPollingIntervalSeconds: 10,
+        localConcurrency: options?.concurrency ?? 10,
+      },
+      (jobs) => handler(jobs.map((j) => j.data)),
+    );
+  }
+  async createJobQueue(name: string): Promise<void> {
+    await this.boss.createQueue(name, {
+      notify: true,
+      retryLimit: Infinity,
+      retryBackoff: true,
+    });
+  }
+}
