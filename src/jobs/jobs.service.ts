a12128f29fe95a538f78cd95f9ad1c92b16f64bf
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-30 14:14:20 +0300

    feat: add IJobsService base class

---
+++ src/jobs/jobs.service.ts
@@ -0,0 +1,31 @@
+export type JobData = Record<string, unknown>;
+export type WorkerId = string;
+export type JobId = string;
+export type CreateWorkerOptions = { concurrency?: number; batchSize?: number };
+export abstract class IJobsService {
+  /**
+   * Used to send new jobs with their data to a specified `jobKind` (or queue name).
+   * @param jobKind
+   * @param data
+   */
+  abstract sendJob<T extends JobData>(
+    jobKind: string,
+    data: T,
+  ): Promise<JobId | null>;
+  /**
+   * Create new worker on a specified existed `jobKind` (queue name).
+   * @param jobKind
+   * @param handler
+   */
+  abstract createWorker<T extends JobData>(
+    jobKind: string,
+    handler: (data: T[]) => Promise<void>,
+    options?: CreateWorkerOptions,
+  ): Promise<WorkerId>;
+
+  /**
+   * Responsible for creating new queue (new kind of jobs) with a unique given name.
+   * @param name
+   */
+  abstract createJobQueue(name: string): Promise<void>;
+}
