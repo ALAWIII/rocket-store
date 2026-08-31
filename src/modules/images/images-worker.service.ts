b15eeede928738eb356b096515d24f0b82e57a35
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-08-31 10:56:24 +0300

    feat: add ImagesWorkerService initalization startup logic.

---
+++ src/modules/images/images-worker.service.ts
@@ -0,0 +1,44 @@
+import { DeleteObjectsCommand } from '@aws-sdk/client-s3';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { IJobsService } from 'src/jobs/jobs.service';
+import { ObjectStorageS3Client } from 'src/object-storage/object-storage.s3-client';
+
+export type ImageDeletionPayload = { Key: string };
+
+@Injectable()
+export class ImagesWorkerService implements OnModuleInit {
+  constructor(
+    private readonly s3Client: ObjectStorageS3Client,
+    private readonly jobService: IJobsService,
+  ) {}
+  async onModuleInit() {
+    await this.jobService.createJobQueue('image.delete');
+    for (let x = 1; x <= 5; x++) {
+      await this.jobService.createWorker<ImageDeletionPayload>(
+        'image.delete',
+        (d) => this.deleteImages(d),
+      );
+    }
+  }
+  private async deleteImages(imgs: ImageDeletionPayload[]) {
+    if (!imgs.length) return;
+    const chunks = this.chunk(imgs, 1000);
+    await Promise.all(
+      chunks.map((chunk) =>
+        this.s3Client.getClient().send(
+          new DeleteObjectsCommand({
+            Bucket: 'images',
+            Delete: { Objects: chunk },
+          }),
+        ),
+      ),
+    );
+  }
+  private chunk<T>(arr: T[], size: number): T[][] {
+    const out: T[][] = [];
+    for (let i = 0; i < arr.length; i += size) {
+      out.push(arr.slice(i, i + size));
+    }
+    return out;
+  }
+}
--- src/modules/images/images.module.ts
+++ src/modules/images/images.module.ts
@@ -5,10 +5,12 @@ import { ImagesService } from './images.service';
 import { IImageRepository } from './infrastructure/repositories/image.repository';
 import { ImageRepository } from './infrastructure/repositories/image.typeorm.repository';
 import { ImagesStorageService } from './images.storage.service';
+import { ImagesWorkerService } from './images-worker.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ImageEntity])],
   providers: [
+    ImagesWorkerService,
     ImagesService,
     ImagesStorageService,
     { provide: IImageRepository, useClass: ImageRepository },
