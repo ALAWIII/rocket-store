d6342d5672164da2a82bfd7eb55593b4ddc8960f
allawiii <alighraibeh87@gmail.com>
2026-09-14 18:47:39 +0300

    feat: implement findUnused method

--- src/modules/images/images.controller.ts
+++ src/modules/images/images.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe, Query } from '@nestjs/common';
 import { RequirePermission } from '../shared/authorization/decorators/require-permission.decorator';
 import { ImagesService } from './images.service';
 import { AllPermissions } from '../access-control/domain/permission';
 import { ImageResponseDto } from './dto/image-response.dto';
+import { FindUnusedImagesDto } from './dto/find-unused-images-pagination.dto';
+import { FindUnusedImagesResponseDto } from './dto/find-unused-images-response.dto';
 
 @Controller('images')
 export class ImagesController {
@@ -13,5 +15,18 @@ export class ImagesController {
     @Param('id', new ParseUUIDPipe({ version: '7' })) id: string,
   ): Promise<ImageResponseDto> {
     return (await this.imagesService.findImageById(id)).unwrap().toJSON();
+  }
+  @Get()
+  @RequirePermission(AllPermissions.images.ImagesReadAny)
+  async findUnused(
+    @Query() filters: FindUnusedImagesDto,
+  ): Promise<FindUnusedImagesResponseDto> {
+    const findImgs = (
+      await this.imagesService.findUnUsedImages(filters)
+    ).unwrap();
+    return {
+      pagination: findImgs.pagination,
+      images: findImgs.images.map((img) => img.toJSON()),
+    };
   }
 }
