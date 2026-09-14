553447da0741fa4931bae6757e3eb7b66a04009d
allawiii <alighraibeh87@gmail.com>
2026-09-14 16:39:56 +0300

    feat: add empty images controller.

--- src/modules/images/images.module.ts
+++ src/modules/images/images.module.ts
@@ -6,6 +6,7 @@ import { IImageRepository } from './infrastructure/repositories/image.repository
 import { ImageRepository } from './infrastructure/repositories/image.typeorm.repository';
 import { ImagesStorageService } from './images.storage.service';
 import { ImagesWorkerService } from './images-worker.service';
+import { ImagesController } from './images.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ImageEntity])],
@@ -15,5 +16,6 @@ import { ImagesWorkerService } from './images-worker.service';
     ImagesStorageService,
     { provide: IImageRepository, useClass: ImageRepository },
   ],
+  controllers: [ImagesController],
 })
 export class ImagesModule {}
