addb367f9ab0c188e8cbbaa3ac93e62e6ae2ae98
allawiii <alighraibeh87@gmail.com>
2026-09-14 18:05:53 +0300

    feat: implement findById method.

--- src/modules/images/images.controller.ts
+++ src/modules/images/images.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { RequirePermission } from '../shared/authorization/decorators/require-permission.decorator';
+import { ImagesService } from './images.service';
+import { AllPermissions } from '../access-control/domain/permission';
+import { ImageResponseDto } from './dto/image-response.dto';
 
 @Controller('images')
-export class ImagesController {}
+export class ImagesController {
+  constructor(private readonly imagesService: ImagesService) {}
+  @Get(':id')
+  @RequirePermission(AllPermissions.images.ImagesReadAny)
+  async findById(
+    @Param('id', new ParseUUIDPipe({ version: '7' })) id: string,
+  ): Promise<ImageResponseDto> {
+    return (await this.imagesService.findImageById(id)).unwrap().toJSON();
+  }
+}
