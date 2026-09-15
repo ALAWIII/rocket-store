f741da83dedaea3cfcc1cd19b670cac61c2198a4
allawiii <alighraibeh87@gmail.com>
2026-09-15 17:06:05 +0300

    refactor: remove unneeded type.

--- src/modules/shared/types/image-entities.type.ts
+++
@@ -1,9 +0,0 @@
-export const IMAGE_ENTITY_TYPES = [
-  'PRODUCT',
-  'PRODUCT_VARIANT',
-  'BRAND',
-  'CATEGORY',
-  'USER',
-] as const;
-
-export type ImageEntityType = (typeof IMAGE_ENTITY_TYPES)[number];
