abb4d8c01898bd2fb85b3b1baa6ef5aeec993d47
allawiii <alighraibeh87@gmail.com>
2026-09-15 16:38:39 +0300

    feat: implement upload image method endpoint.

--- src/modules/images/images.controller.ts
+++ src/modules/images/images.controller.ts
@@ -5,7 +5,10 @@ import {
   Get,
   Param,
   ParseUUIDPipe,
+  Post,
   Query,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { RequirePermission } from '../shared/authorization/decorators/require-permission.decorator';
 import { ImagesService } from './images.service';
@@ -15,10 +18,32 @@ import { FindUnusedImagesDto } from './dto/find-unused-images-pagination.dto';
 import { FindUnusedImagesResponseDto } from './dto/find-unused-images-response.dto';
 import { RemoveImagesDto } from './dto/remove-images.dto';
 import { RemoveImagesResponseDto } from './dto/remove-images-response.dto';
-
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Session } from '@thallesp/nestjs-better-auth';
+import type { AppSession } from 'src/auth/auth.config';
+import { UploadFileInfoDto } from './dto/upload-file-info.dto';
+import { fileFilter } from './util/file-filter.util';
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
+
+  @Post()
+  @RequirePermission(AllPermissions.images.ImagesUploadAny)
+  @UseInterceptors(
+    FileInterceptor('file', {
+      limits: { fileSize: 10 * 1024 * 1024 },
+      fileFilter: fileFilter,
+    }),
+  )
+  async upload(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() fileInfo: UploadFileInfoDto,
+    @Session() session: AppSession,
+  ): Promise<ImageResponseDto> {
+    return (await this.imagesService.upload(file, session.user.id, fileInfo))
+      .unwrap()
+      .toJSON();
+  }
   @Get(':id')
   @RequirePermission(AllPermissions.images.ImagesReadAny)
   async findById(
