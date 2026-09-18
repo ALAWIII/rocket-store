3f0a2a10967c71faab179da8a6b24b0378da49b2
allawiii <alighraibeh87@gmail.com>
2026-09-18 12:12:55 +0300

    feat: add CreateBrandDto

---
+++ src/modules/brands/dto/create-brand.dto.ts
@@ -0,0 +1,9 @@
+import { IsString, Length, Matches } from 'class-validator';
+import { brandRegex } from 'src/modules/shared/value-objects/brand-name';
+
+export class CreateBrandDto {
+  @IsString()
+  @Length(2, 50)
+  @Matches(brandRegex, { message: 'Invalid brand name' })
+  name!: string;
+}
