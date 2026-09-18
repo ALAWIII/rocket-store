494aadfdcbf2e1d65f2315296a479cbd29f1f24f
allawiii <alighraibeh87@gmail.com>
2026-09-18 19:31:55 +0300

    feat: implement create brand endpoint method.

--- src/modules/brands/brands.controller.ts
+++ src/modules/brands/brands.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RequirePermission } from '../shared/authorization/decorators/require-permission.decorator';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { BrandsService } from './brands.service';
+import { BrandResponseDto } from './dto/brand-response.dto';
+import { AllPermissions } from '../access-control/domain/permission';
 
 @Controller('brands')
-export class BrandsController {}
+export class BrandsController {
+  constructor(private readonly brandService: BrandsService) {}
+  @Post()
+  @RequirePermission(AllPermissions.brands.BrandsCreateAny)
+  async create(@Body() brand: CreateBrandDto): Promise<BrandResponseDto> {
+    return this.brandService.createBrand(brand);
+  }
+}
