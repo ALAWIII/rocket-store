9b159df14d39df5abd1a398daf6faf070951a836
allawiii <alighraibeh87@gmail.com>
2026-09-18 19:05:12 +0300

    feat: import BrandsController into the BrandsModule.

--- src/modules/brands/brands.module.ts
+++ src/modules/brands/brands.module.ts
@@ -5,6 +5,7 @@ import { BrandImagesEntity } from './infrastructure/entities/brand-images.entity
 import { BrandsService } from './brands.service';
 import { IBrandRepository } from './infrastructure/repositories/brand.repository';
 import { BrandRepository } from './infrastructure/repositories/brand.typeorm.repository';
+import { BrandsController } from './brands.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([BrandEntity, BrandImagesEntity])],
@@ -12,5 +13,6 @@ import { BrandRepository } from './infrastructure/repositories/brand.typeorm.rep
     { provide: IBrandRepository, useClass: BrandRepository },
     BrandsService,
   ],
+  controllers: [BrandsController],
 })
 export class BrandsModule {}
