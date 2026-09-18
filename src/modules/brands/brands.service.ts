b52b42abf1b7e3a7f52c864d173c6c1f53be28ba
allawiii <alighraibeh87@gmail.com>
2026-09-18 15:08:01 +0300

    feat: implement renameBrand service method.

--- src/modules/brands/brands.service.ts
+++ src/modules/brands/brands.service.ts
@@ -3,6 +3,7 @@ import { IBrandRepository } from './infrastructure/repositories/brand.repository
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { Brand } from './domain/brand';
 import { BrandResponseDto } from './dto/brand-response.dto';
+import { RenameBrandDto } from './dto/rename-brand.dto';
 
 @Injectable()
 export class BrandsService {
@@ -10,5 +11,11 @@ export class BrandsService {
   async createBrand(brandData: CreateBrandDto): Promise<BrandResponseDto> {
     const brand = Brand.create({ name: brandData.name }).unwrap();
     return (await this.brandRepo.create(brand)).unwrap().toJSON();
+  }
+  async renameBrand(
+    brandId: string,
+    name: RenameBrandDto,
+  ): Promise<BrandResponseDto> {
+    return (await this.brandRepo.rename(brandId, name.name)).unwrap().toJSON();
   }
 }
